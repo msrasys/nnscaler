4d2b9b8baf60e9ce61c5c5d420bb6ad99ce9c133
Zhiqi Lin <v-zhiql@microsoft.com>
2021-07-01 12:08:34 +0000

    maintainable

--- scripts/keep.py
+++ scripts/keep.py
@@ -1,18 +1,29 @@
 import torch
 import time
-
-interval = 2
-
-torch.cuda.set_device(3)
-a = torch.rand((8192, 8192)).cuda()
-b = torch.rand((8192, 8192)).cuda()
+import argparse
 
 
-while True:
-    tic = time.time()
-    for _ in range(5000):
-        c = a * b
-    torch.cuda.synchronize()
-    toc = time.time()
-    print('time span: {}s'.format(toc - tic))
-    time.sleep(interval)
+def keep(rank, args):
+
+    torch.cuda.set_device(rank)
+    a = torch.rand((8192, 8192)).cuda()
+    b = torch.rand((8192, 8192)).cuda()
+
+    while True:
+        tic = time.time()
+        for _ in range(5000):
+            c = a * b
+        torch.cuda.synchronize()
+        toc = time.time()
+        if rank == 0:
+            print('time span: {}s'.format(toc - tic))
+        time.sleep(args.interval)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--interval', type=int, default=2)
+    parser.add_argument('--gpus', type=int, default=1)
+    args = parser.parse_args()
+
+    torch.multiprocessing.spawn(keep, args=(args,), nprocs=args.gpus, join=True)
