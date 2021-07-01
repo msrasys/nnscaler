d28543699acb01d3f167ce2be491df8dcd43de6b
Zhiqi Lin <v-zhiql@microsoft.com>
2021-07-01 12:43:25 +0000

    work in progress: enable linear tensor partition

---
+++ combo/physical/device/group.py
@@ -0,0 +1,11 @@
+"""
+Communication group settings among devices
+"""
+
+import torch
+
+def create_group(device_count):
+    """
+    Create device group
+    """
+    pass
--- combo/physical/operator/linear.py
+++ combo/physical/operator/linear.py
@@ -3,6 +3,7 @@ import torch
 from torch import Tensor
 from torch.overrides import has_torch_function_variadic, handle_torch_function
 
+
 def linear_op(input: Tensor, weight: Tensor, bias: Optional[Tensor] = None) -> Tensor:
     r"""
     Applies a linear transformation to the incoming data: :math:`y = xA^T + b`.
@@ -20,4 +21,29 @@ def linear_op(input: Tensor, weight: Tensor, bias: Optional[Tensor] = None) -> T
     if has_torch_function_variadic(input, weight):
         print('note: this branch should not pass')
         return handle_torch_function(linear, (input, weight), input, weight, bias=bias)
-    return torch._C._nn.linear(input, weight, bias)
+
+    # single GPU version
+    if True:
+        output = torch._C._nn.linear(input, weight, bias)
+
+    # multi-GPU version 
+    #   - Assume input is full
+    #   - split cloumn: Y = XA + b where A = [A_1, ..., A_p]
+    elif True:
+        # forward: identity; backward: allreduce
+        input = copy_to_tensor_model_parallel_region(input)
+        output = torch._C._nn.linear(input, weight, bias)
+        # forward: allgather; backward: scatter
+        output = gather_from_tensor_model_parallel_region(output)
+    
+    # multi-GPU version
+    #   - Assume input is full
+    #   - split row: Y = XA + b where X = [X_1, ..., X_p], A = [A_1 || ... || A_p]
+    elif True:
+        # forward: scatter; backward: allgather
+        input = scatter_to_tensor_model_parallel_region(input)
+        output = torch._C._nn.linear(input, weight, bias)
+        # forward: reduce; backward: identity
+        output = reduce_from_tensor_model_parallel_region(output)
+
+    return output
