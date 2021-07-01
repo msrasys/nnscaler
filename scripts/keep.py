ccb32649ebf77f43776d29ba9cd4f95c4c7a1c58
Zhiqi Lin <v-zhiql@microsoft.com>
2021-07-01 12:37:41 +0000

    auto detect gpu util

--- scripts/keep.py
+++ scripts/keep.py
@@ -2,6 +2,32 @@ import torch
 import time
 import argparse
 
+import subprocess
+import re
+
+def get_gpu_util(rank):
+
+    cmds = [
+        'nvidia-smi',
+        '-i',
+        str(rank),
+    ]
+    proc = subprocess.Popen(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    outputs = stdout.decode('utf-8').split('\n')
+    
+    util = 0
+    for output in outputs[::-1]:
+        # switch to performance line
+        if 'Default' in output:
+            # match all the numbers and return the last one
+            util = re.findall(r'\d+', output)[-1]
+            util = int(util)
+            break
+    else:
+        print("rank {}: couldn't match any, check GPU status!".format(rank))
+    return util
+
 
 def keep(rank, args):
 
@@ -16,8 +42,16 @@ def keep(rank, args):
         torch.cuda.synchronize()
         toc = time.time()
         if rank == 0:
-            print('time span: {}s'.format(toc - tic))
+            print('benchmark 8K matmul: time span: {}ms'.format((toc - tic) * 1000 / 5000))
         time.sleep(args.interval)
+        while True:
+            util = get_gpu_util(rank)
+            if util >= 0:
+                break
+            print('rank {}: find gpu busy, keep sleeping...'.format(rank))
+            time.sleep(args.interval)
+        print('rank {} gets up'.format(rank))
+
 
 if __name__ == '__main__':
 
