700994446905621e876eb49d6cab79c496acfbd5
Zhiqi Lin <v-zhiql@microsoft.com>
2021-07-01 10:38:27 +0000

    setup maintain scripts

--- scripts/env-setup.sh
+++ scripts/env-setup.sh
@@ -5,14 +5,14 @@ git config --global core.editor "vim"
 git config --global user.name "Zhiqi Lin"
 git config --global user.email "v-zhiql@microsoft.com"
 
-git config --global core.editor "vim"
-git config --global user.name "Zhiqi Lin"
-git config --global user.email "v-zhiql@microsoft.com"
+sudo git config --global core.editor "vim"
+sudo git config --global user.name "Zhiqi Lin"
+sudo git config --global user.email "v-zhiql@microsoft.com"
+sudo chmod -R a+w /opt/conda
 
-
-cd /root
-wget https://raw.githubusercontent.com/zhiqi-0/EnvDeployment/master/.tmux.conf
-wget https://raw.githubusercontent.com/zhiqi-0/EnvDeployment/master/.vimrc
+sudo apt-get install tmux -y
+wget https://raw.githubusercontent.com/zhiqi-0/EnvDeployment/master/.tmux.conf -O ~/.tmux.conf
+wget https://raw.githubusercontent.com/zhiqi-0/EnvDeployment/master/.vimrc -O ~/.vimrc
 
 echo 'export PATH=/opt/conda/bin:$PATH' >> ~/.bashrc
 echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
--- scripts/keep.py
+++ scripts/keep.py
@@ -1,13 +1,18 @@
 import torch
 import time
 
-interval = 10
+interval = 2
 
+torch.cuda.set_device(3)
 a = torch.rand((8192, 8192)).cuda()
 b = torch.rand((8192, 8192)).cuda()
 
 
 while True:
-    for _ in range(1000):
+    tic = time.time()
+    for _ in range(5000):
         c = a * b
+    torch.cuda.synchronize()
+    toc = time.time()
+    print('time span: {}s'.format(toc - tic))
     time.sleep(interval)
