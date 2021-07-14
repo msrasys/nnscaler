3589f1310b712468f62dcabc5bd7fe8fd52c5169
Zhiqi Lin <v-zhiql@microsoft.com>
2021-07-14 07:10:11 +0000

    update virtual community interface

--- combo/tensor/logic/community.py
+++ combo/tensor/logic/community.py
@@ -1,3 +1,34 @@
+import torch
+
+
+__all__ = ['ReductionOpPool', 'Community']
+
+
+class _Reduction(type):
+
+    Sum = torch.distributed.all_reduce
+
+    # identity for replica
+    Replica = lambda physical_tensor, group : physical_tensor
+
+    def register(name, udf):
+        """
+        Reduction functions should be in function format:
+
+        Arguments:
+            PhysicalTensor
+            Communication Group
+
+        Return:
+            PhysicalTensor
+        """
+        if hasattr(cls, name):
+            raise KeyError("{} is registered".format(name))
+        setattr(cls, name, udf)
+
+
+class ReductionOpPool(metaclass=_Reduction):
+    pass
 
 
 class Community:
@@ -16,18 +47,31 @@ class Community:
         self.reduction = reduction
 
     def spread(self, device_list):
-        """Create physical tensors and spread to devices
+        """Spread physical tensors to devices
+    
+        Create physical tensors for this community and spread out
+        based on the given device list.
+
+        This offers policy module an interface to decide which devices
+        to spread.
 
         Argument:
             device_list (list[int]): device id list
         
         Return:
+            PhysicalTensor(s) or None:
 
+                For SPMD programming model:
+                    if current device is in the `device_list`,
+                        than return the corresponding physical tensor,
+                    else None
+
+                For Global View programming model:
+                    return list[PhysicalTensor] with the same
+                    order of `device_list`.
         """
         pass
 
-    def fuse(self):
-        """Fuse the spread physical tensors into the one
-        Perform reduction function to get the results on each physical tensor
-        """
+    def sync(self):
+        """Synchrnoize the spread physical tensors by reduction operation"""
         pass
