552b1077bf759dea8ef080564042ef548bdc4e30
Zhiqi Lin <v-zhiql@microsoft.com>
2021-07-16 06:50:26 +0000

    two stage for segmentation

---
+++ cube/tensor/logic/segment/outline.py
@@ -0,0 +1,52 @@
+"""
+This is the description interface to describe the 
+segementation requirement (restrictions).
+
+The description includes two parts:
+
+    1). restriction description on tensor segementation
+
+    2). Translation procedure in runtime to translate such a restriction
+        to the real segmentation on given logical tensor shape.
+"""
+
+
+# primitives to describe segmentation pattern
+
+class SplitAxis:
+
+    def __init__(self, axis, chunk_num=None, chunk_size=None, overlap=0):
+        """
+        Segmentation Pattern Requirement (parameters):
+
+        axis (int): the axis to split
+
+        chunk_num (None, int, tuple(int, int)):
+            valid chunk numbers to split.
+            If None, then any chunk number is valid;
+            If an integer, only the specified chunk number is valid;
+            If a tuple(min, max), the chunk number wihtin the scope [min,max] is valid
+
+        chunk_size (None, int, tuple(int, int)):
+            valid chunk size.
+            If None, any size is valid;
+            If an integer, each chunk size is valid;
+            if a tuple(min, max), the chunk size wihtin the scope [min,max] is valid
+        
+        overlap (0, int, tuple(int, int)):
+            valid size for overlaping on the boundary of each splitted chunks.
+            If None, any overlapping is valid
+            If an integer, each overlap size is valid;
+            if a tuple(min, max), the overlap size wihtin the scope [min,max] is valid
+
+        """
+        self.axis = axis
+        self.chunk_num = chunk_num
+        self.chunk_size = chunk_size
+        self.overlap = overlap
+    
+    def __call__(self, shape):
+        """
+        Runtime community generation given the logical tensor shape
+        """
+        pass
--- cube/tensor/logic/segment/primitive.py
+++ cube/tensor/logic/segment/primitive.py
@@ -1,47 +1,66 @@
 """
-This is the primitive to describe the layout requirement.
-
-The primitive translates the requirement before runtime to the 
-actual community groups during the runtime
+This is the runtime primitive sets to setup community for a logical tensor.
 """
 
 
-# primitives to describe segmentation pattern
+## Basic interface to cover all the cases
+class DataSegment:
+    """
+    The basic primitive to gather data in the logical tensor.
 
-class SplitAxis:
+    The order of indices indicate the physical storage (1-D array) order
+    """
 
-    def __init__(self, axis, chunk_num=None, chunk_size=None, overlap=0):
+    def __init__(self, indices_list=None):
         """
-        Segmentation Pattern Requirement (parameters):
-
-        axis (int): the axis to split
-
-        chunk_num (None, int, tuple(int, int)):
-            valid chunk numbers to split.
-            If None, then any chunk number is valid;
-            If an integer, only the specified chunk number is valid;
-            If a tuple(min, max), the chunk number wihtin the scope [min,max] is valid
-
-        chunk_size (None, int, tuple(int, int)):
-            valid chunk size.
-            If None, any size is valid;
-            If an integer, each chunk size is valid;
-            if a tuple(min, max), the chunk size wihtin the scope [min,max] is valid
-        
-        overlap (0, int, tuple(int, int)):
-            valid size for overlaping on the boundary of each splitted chunks.
-            If None, any overlapping is valid
-            If an integer, each overlap size is valid;
-            if a tuple(min, max), the overlap size wihtin the scope [min,max] is valid
-
+        Args:
+            indices_list (list[ list[int], ]):
+                List of index
         """
-        self.axis = axis
-        self.chunk_num = chunk_num
-        self.chunk_size = chunk_size
-        self.overlap = overlap
+
+        self.indices = indices_list
+
+    def convert_to_indices(self):
+        """
+        Convert to index list
+        """
+        pass
+
+    def reorder(self, new_orders):
+        """
+        Reorder the indices.
+
+        Note this can be only called before materialize physical tensors,
+        or called from underlying operation that will change physical storage format
+        """
+        #TODO: validation check
+        self.indices = new_orders
+
+
+## Higher level interface to cover the most cases ##
+class TileSegment(DataSegment):
+    """
+    A tile is a contigonous block on the logical tensor shape,
+    which can be represented as the start position + offset (shape)
+    """
+
+    def __init__(self, anchor, offset):
+        """
+        Args:
+            anchor (list[int]): start position of the tile
+            offset (list[int]): offset (shape) of the tile
+        """
+        if len(anchor) != len(offset):
+            raise ValueError("Require anchor length to be equal with offset length")
+        super().__init__()
+        self.anchor = anchor
+        self.offset = offset
     
-    def __call__(self, shape):
+    def convert_to_indices(self):
         """
-        Runtime community generation given the logical tensor shape
+        Convert anchor and offset to index list
         """
+        pass
+
+    def reorder(self):
         pass
