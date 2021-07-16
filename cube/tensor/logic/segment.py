96c854db9c1f02bfd9c8a4bdc8310f756f62c5bb
Zhiqi Lin <v-zhiql@microsoft.com>
2021-07-16 03:18:41 +0000

    add example primitives for splitting axis

--- cube/tensor/logic/segment.py
+++ cube/tensor/logic/segment.py
@@ -3,9 +3,8 @@ This is the interface for describing which set of data is needed for
 gathering a community.
 """
 
+
 ## Basic interface to cover all the cases
-
-
 class DataSegment:
     """
     The basic primitive to gather data in the logical tensor.
@@ -40,7 +39,6 @@ class DataSegment:
 
 
 ## Higher level interface to cover the most cases ##
-
 class TileSegment(DataSegment):
     """
     A tile is a contigonous block on the logical tensor shape,
@@ -67,5 +65,44 @@ class TileSegment(DataSegment):
 
     def reorder(self):
         pass
-    
 
+
+# primitives to describe segmentation pattern
+
+class SplitAxis:
+
+    def __init__(self, axis, chunk_num=None, chunk_size=None, overlap=0):
+        """
+        Segmentation Pattern Requirement (parameters):
+
+        axis (int): the axis to split
+
+        chunk_num (None, int, tuple(int, int)):
+            valid chunk numbers to split.
+            If None, then any chunk number is valid;
+            If an integer, only the specified chunk number is valid;
+            If a tuple(min, max), the chunk number wihtin the scope [min,max] is valid
+
+        chunk_size (None, int, tuple(int, int)):
+            valid chunk size.
+            If None, any size is valid;
+            If an integer, each chunk size is valid;
+            if a tuple(min, max), the chunk size wihtin the scope [min,max] is valid
+        
+        overlap (0, int, tuple(int, int)):
+            valid size for overlaping on the boundary of each splitted chunks.
+            If None, any overlapping is valid
+            If an integer, each overlap size is valid;
+            if a tuple(min, max), the overlap size wihtin the scope [min,max] is valid
+
+        """
+        self.axis = axis
+        self.chunk_num = chunk_num
+        self.chunk_size = chunk_size
+        self.overlap = overlap
+    
+    def __call__(self, shape):
+        """
+        Runtime community generation given the logical tensor shape
+        """
+        pass
