f38d1a0892a4663fe2763f06dbfc037a4b28ebda
Zhiqi Lin <v-zhiql@microsoft.com>
2021-07-26 07:44:45 +0000

    add placement to physcial op

--- cube/operator/physic/generics.py
+++ cube/operator/physic/generics.py
@@ -2,34 +2,85 @@
 This should be the interface with C level kernel launch
 """
 
+from cube.device.physic.group import DeviceGroup
 import torch
+
+class OpResult:
+    """
+    The empty result is used for re-constructing community
+    """
+    def __init__(self, result, ranks):
+        self.res = result
+        self.placement = ranks
+    
+    def get_result(self):
+        return self.res
 
 
 class GenericPhysicOp:
+    """
+    The generic physical op takes at least one physical tensor,
+    and generates at least one physical tensor.
+
+    If there is no tensor as input, will return an empty result
+    which indicates which rank will generate the correct one.
+    """
 
     def __init__(self, func, placement=None):
         
         if not callable(func):
             raise TypeError("Expect callable function")
         self.func = [func]
-        self.placement = placement
+        self._placement = placement
+        self.execute_flag = False
+        self.policy_fn = None
     
-    def set_placement(self, placement):
-        if not isinstance(placement, torch.device):
-            raise TypeError("Expected torch device")
-        self.placement = placement
+    @property
+    def placement(self):
+        """
+        Ranks for the op to execute
+        """
+        return self._placement
+
+    @placement.setter
+    def placement(self, ranks):
+        if not isinstance(ranks, list):
+            raise TypeError("Expected list of int ranks")
+        self._placement = ranks
+        if DeviceGroup().rank not in self.placement:
+            self.execute_flag = False
+        else:
+            self.execute_flag = True
+
+    def register_policy(self, policy_fn):
+        if not callable(policy_fn):
+            raise TypeError("Expected callable policy function")
+        self.policy_fn = [policy_fn]
 
     def __call__(self, *args, **kwargs):
+        #TODO: fix for model-partition with send/recv
+        if self.placement is None:
+            if self.policy_fn is None:
+                #TODO: fix: this will break between-device consistency view
+                self.placement = [torch.cuda.current_device()]
+            else:
+                self.placement = self.policy_fn(*args, **kwargs)
+        if not self.execute_flag:
+            return OpResult(None, self.placement)
 
         # tensor movement
         for arg in args:
             if torch.is_tensor(arg):
-                if arg.device != self.placement:
-                    arg.data = arg.detach().to(self.placement)
+                #TODO: rank -> device mapping, send/recv
+                if arg.device.index not in self.placement:
+                    #TODO: rank -> device mapping, send/recv
+                    arg.data = arg.detach().cuda()
         for key in kwargs:
             if torch.is_tensor(kwargs[key]):
-                if kwargs[key].device != self.placement:
-                    kwargs[key].data = kwargs[key].detach().to(self.placement)
+                #TODO: rank -> device mapping, send/recv
+                if kwargs[key].device.index not in self.placement:
+                    # TODO: rank -> device mapping, send/recv
+                    kwargs[key].data = kwargs[key].detach().cuda()
 
         outputs = self.func[0](*args, **kwargs)
-        return outputs
+        return OpResult(outputs, self.placement)
--- tests/operator/test_physic_op.py
+++ tests/operator/test_physic_op.py
@@ -1,21 +1,47 @@
-from cube.operator.physic.generics import GenericPhysicOp
+"""
+cmd for running the test
+
+python -m torch.distributed.launch \
+    --nproc_per_node=4 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=62000 \
+    --use_env \
+    tests/operator/test_physic_op.py
+"""
+
+from cube.device.physic.group import DeviceGroup
+from cube.operator.physic.generics import GenericPhysicOp, OpResult
 import torch
 
 
 def test_physic_generic_op():
 
+    myrank = DeviceGroup().rank
+    ranks = [0, 2]
+
     op = GenericPhysicOp(torch._C._nn.linear)
     assert op.placement is None
-
-    op.set_placement(torch.device('cuda:0'))
+    
+    op.placement = ranks
+    assert op.func[0] is torch._C._nn.linear
+    assert op.placement == [0, 2]
+    assert op.execute_flag == (myrank in ranks)
     
     matA = torch.randn((1024,1024))
     matB = torch.randn((1024,1024))
     matC = op(matA, matB, bias=None)
-    assert matC.device == torch.device('cuda:0')
+    
+    assert set(matC.placement) == set(ranks)
+    if myrank in ranks:
+        assert torch.is_tensor(matC.get_result())
+    else:
+        assert matC.get_result() is None
 
     matC_ref = torch._C._nn.linear(matA, matB, bias=None)
-    assert torch.allclose(matC, matC_ref) is True
+    if myrank in ranks:
+        assert torch.allclose(matC.get_result(), matC_ref) is True
 
 
 if __name__ == '__main__':
