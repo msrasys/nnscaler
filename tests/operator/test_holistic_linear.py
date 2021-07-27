9bbf799df28db65dd7995a1f61b683facefeae82
Zhiqi Lin <v-zhiql@microsoft.com>
2021-07-27 13:15:32 +0000

    POC to find hardware bias

--- tests/operator/test_holistic_linear.py
+++ tests/operator/test_holistic_linear.py
@@ -20,7 +20,7 @@ from cube.operator.holist.linear import LinearColumnInputRowWeight
 from cube.device.physic.group import DeviceGroup
 
 import torch
-torch.manual_seed(121)
+torch.manual_seed(100)
 
 
 class LogicalLinear:
@@ -37,16 +37,44 @@ class LogicalLinear:
         return [output_shape]
 
 
-def test_holistic_linear_op_column_weight():
+def test_linear_POC():
 
+    N = 1024
+    input = torch.randn((1024, 1024)).cuda()
+    weight = torch.randn((N, 1024))
+    bias = torch.randn((N,))
+
+    rank = DeviceGroup().rank
+
+    # partial
+    partial_weight = torch.chunk(weight, 4, dim=0)[rank].cuda()
+    partial_bias = torch.chunk(bias, 4, dim=0)[rank].cuda()
+    partial_out = torch._C._nn.linear(input, partial_weight, partial_bias)
+
+    # full
+    out_full = torch._C._nn.linear(input, weight.cuda(), bias.cuda())
+    ref_out = torch.chunk(out_full, 4, dim=1)[rank].cuda()
+
+    if rank == 0:
+        print('max bias: ', torch.max(torch.abs(partial_out - ref_out)))
+        print('sum bias: ', torch.sum(torch.abs(partial_out - ref_out)))
+
+
+def test_holistic_linear_op_column_weight():
+    """
+    Note: Due to unknown reason in hardware, the output will have up to
+    0.0001 bias. This is verified in `test_linear_POC()` The larger 
+    K results larger bias.
+    """
+    N = 1024
     input = LogicalTensor(shape=(1024,1024))
-    weight = LogicalTensor(shape=(1024,1024))
-    bias = LogicalTensor(shape=(1024,))
+    weight = LogicalTensor(shape=(N,1024))
+    bias = LogicalTensor(shape=(N,))
 
     holistic_op = LinearColumnWeight()
     holistic_op.logical_op = LogicalLinear()
 
-    # policy setup
+    # ================================ Policy ===========================
     def policy_for_how_many_tiles(outliner):
         if isinstance(outliner, sg.outline.Full):
             pass
@@ -71,20 +99,29 @@ def test_holistic_linear_op_column_weight():
     holistic_op.set_segmentation_policy(
         policy_for_how_many_tiles
     )
+    # ================================ Policy ===========================
 
     output = holistic_op(input, weight, bias)
 
-    output_ref = torch._C._nn.linear(input.data.cuda(), weight.data.cuda(), bias.data.cuda())
+    # =============================== Test ==============================
+    output_ref = torch._C._nn.linear(
+        input.data.cuda(), weight.data.cuda(), bias.data.cuda()
+    )
     rank = DeviceGroup().rank
     output_ref = torch.chunk(output_ref, chunks=4, dim=1)[rank].contiguous()
     out = output.get_physical_tensor(rank)
-    if rank == 0:
-        print('ref: ', output_ref)
-        print('get: ', out)
-        print('sum: ', torch.sum(torch.abs(out - output_ref)))
-    print(torch.allclose(output.get_physical_tensor(rank), output_ref)) # is True
+    # if rank == 0:
+    #     print('ref: ', output_ref)
+    #     print('get: ', out)
+    #     print('max bias: ', torch.max(torch.abs(out - output_ref)))
+    #     print('sum bias: ', torch.sum(torch.abs(out - output_ref)))
+    error_max = torch.max(torch.abs(out - output_ref))
+    assert error_max.item() < 2e-4
+    # =============================== Test ==============================
 
 
 if __name__ == '__main__':
-
+    group = DeviceGroup()
+    
+    # test_linear_POC()
     test_holistic_linear_op_column_weight()
