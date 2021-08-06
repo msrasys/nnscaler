a2f328f95d11369ec3209a6a47203e585509ab50
Zhiqi Lin <v-zhiql@microsoft.com>
2021-08-06 06:55:20 +0000

    parallel primitive update

--- examples/case_study/parallel_primitive.py
+++ examples/case_study/parallel_primitive.py
@@ -13,57 +13,57 @@ def linear_tensor_parallel(inputs, weight, bias):
     K = 1024
     N = 1024
 
-    ### ============ Input Adapter ============ ###
-
-    # select need to consider transformation from one segmentation to another
-    inputs_segment = select(
+    inputs = select(
         tensor = inputs, 
         indices = (slice(0, M), slice(0, K)),
+        val_op = None,
+        ranks = [0, 1, 2, 3],
         shape = (M, K)
     )
 
-    weight_segment = select(
+    weight = select(
         tensor = weight,
         indices = (slice(rank * (N // 4), (rank + 1) * (N // 4)), slice(0, K)),
+        val_op = None,
+        ranks = [0, 1, 2, 3],
         shape = (N // 4, K)
     )
 
-    bias_segment = select(
+    bias = select(
         tensor = bias,
         indices = (slice(rank * (N // 4), (rank + 1) * (N // 4)),),
+        val_op = None,
+        ranks = [0, 1, 2, 3],
         shape = (N // 4,)
     )
 
-    inputs = deploy(
-        segment = inputs_segment,
-        ranks = [0, 1, 2, 3],
-        val_map_op = IdentityForwardAllreduceBackward
-    )
-
-    weight = deploy(
-        segment = weight_segment,
-        ranks = [rank],
-        val_map_op = None
-    )
-
-    bias = deploy(
-        segment = bias_segment,
-        ranks = [rank],
-        val_map_op = None
-    )
-    ### ============ Input Adapter ============ ###
-
-
     ### ============ Compute ============ ###
+    # each rank do this
     output = torch._C._nn.linear(inputs, weight, bias)
     ### ============ Compute ============ ###
 
-
-    ### ============ Output Adapter ============ ###
-    segment = recover(
+    output = merge(
         tensor = output,
         ranks = [0, 1, 2, 3],
-        reduction_op = AllGatherForwardSplitBackward
+        merge_op = all_gather
     )
-    # construct to logical tensor and return
-    ### ============ Output Adapter ============ ###
+
+
+
+    # inputs = deploy(
+    #     segment = inputs_segment,
+    #     ranks = [0, 1, 2, 3],
+    #     val_map_op = IdentityForwardAllreduceBackward
+    # )
+
+    # weight = deploy(
+    #     segment = weight_segment,
+    #     ranks = [rank],
+    #     val_map_op = None
+    # )
+
+    # bias = deploy(
+    #     segment = bias_segment,
+    #     ranks = [rank],
+    #     val_map_op = None
+    # )
