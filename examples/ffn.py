6d114eaebe85885a5d72ac4321403a8e279e6868
Zhiqi Lin <v-zhiql@microsoft.com>
2021-08-09 01:42:10 +0000

    naive torch code for ffn

--- examples/ffn.py
+++ examples/ffn.py
@@ -1,72 +1,33 @@
-"""
-python -m torch.distributed.launch \
-    --nproc_per_node=4 \
-    --nnodes=1 \
-    --node_rank=0 \
-    --master_addr=127.0.0.1 \
-    --master_port=6000 \
-    --use_env \
-    examples/linear.py
-"""
-
 import torch
 from torch import nn
-from torch import Tensor
-from torch.nn.parameter import Parameter
 import torch.nn.functional as F
 
-import math
 import argparse
-
-
-class Linear(nn.Module):
-
-    __constants__ = ['in_features', 'out_features']
-    in_features: int
-    out_features: int
-    weight: Tensor
-
-    def __init__(self, in_features: int, out_features: int, bias: bool = True,
-                 device=None, dtype=None) -> None:
-        factory_kwargs = {'device': device, 'dtype': dtype}
-        super(Linear, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.weight = Parameter(torch.empty((out_features, in_features), **factory_kwargs))
-        if bias:
-            self.bias = Parameter(torch.empty(out_features, **factory_kwargs))
-        else:
-            self.register_parameter('bias', None)
-        self.reset_parameters()
-
-    def reset_parameters(self) -> None:
-        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
-        if self.bias is not None:
-            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
-            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
-            nn.init.uniform_(self.bias, -bound, bound)
-
-    def forward(self, input: Tensor) -> Tensor:
-        return combo_op.linear_op(input, self.weight, self.bias)
 
 
 class FeedForward(nn.Module):
     def __init__(self, dim, dropout=0., mult=16, classes=1000):
         super().__init__()
         self.net = nn.Sequential(
-            Linear(dim, dim * mult),
+            nn.Linear(dim, dim * mult),
             nn.GELU(),
             nn.Dropout(dropout),
-            Linear(dim * mult, dim)
+            nn.Linear(dim * mult, dim)
         )
 
-        self.classifier = Linear(dim, classes)
+        self.classifier = nn.Linear(dim, classes)
 
-    def forward(self, x, labels):
+    def forward(self, x):
         output = self.net(x)
         output = self.classifier(output)
-        loss = F.cross_entropy(output, labels)
-        return loss
+        return output
+
+
+def data_iter(bs, dim, classes, length=64):
+    for _ in range(length):
+        data = torch.randn((bs, dim))
+        label = torch.randint(0, classes, (bs,))
+        yield data, label
 
 
 if __name__ == '__main__':
@@ -78,16 +39,23 @@ if __name__ == '__main__':
     parser.add_argument('--classes', type=int, default=10)
     args = parser.parse_args()
 
-    # init distributed env
-    group = combo.physical.device.group.DeviceGroup()
-    print(group)
-
     model = FeedForward(args.dim, mult=args.heads, classes=args.classes)
     model = model.cuda()
 
-    inputs = torch.rand((args.bs, args.dim)).cuda()
-    labels = torch.randint(0, 10, (args.bs, )).cuda()
-    for _ in range(100):
-        loss = model(inputs, labels)
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=0.001,
+        betas=(0.9, 0.99),
+        weight_decay=0
+    )
+
+    for (data, label) in data_iter(args.bs, args.dim, args.classes):
+        data, label = data.cuda(), label.cuda()
+        # forward
+        output = model(data)
+        loss = F.cross_entropy(output, label)
+        # backward
         loss.backward()
-    print('Done.')
+        # weight update
+        optimizer.step()
+        optimizer.zero_grad()
