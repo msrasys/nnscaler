d11cc3ee515a06cb7003b7cf502e8ac1747e6537
Zhiqi Lin <v-zhiql@microsoft.com>
2021-09-02 06:06:20 +0000

    add gpipe and 1f1b example

--- examples/poc/pipeline_space.py
+++ examples/poc/pipeline_space.py
@@ -2,6 +2,7 @@ from typing import Sequence
 from cube.schedule.action import Action, add_flow
 from cube.schedule.iterator import sequence_space
 from cube.schedule.plan import ExecutionPlan
+from cube.schedule.checker import correct_check
 
 import argparse
 import re
@@ -59,7 +60,7 @@ def print_all_legal_sequence(actions, relations):
     print('total found {} legal sequences'.format(cnt + 1))
 
 
-def fixed_placement_sequence(actions, relations, ndevice, forward, backward):
+def fixed_placement_sequence(actions, relations, ndevice, max_time):
     for cnt, seq in enumerate(sequence_space(actions, relations)):
         # assign device
         for action in seq:
@@ -69,11 +70,83 @@ def fixed_placement_sequence(actions, relations, ndevice, forward, backward):
         execplan.gen()
         iter_time = execplan.get_time()
         print(f'found seq > {seq} \t time {iter_time}')
-        # if iter_time > 28:
-        #     continue
+        if iter_time > max_time:
+            continue
         execplan.draw(outfile='tmp.png')
         input('>>> ')
     print('total found {} legal sequences'.format(cnt + 1))
+
+
+def pipe_1f1b(actions, relations, nstage, ndevice, nmb):
+    num_stage = nstage
+    num_microbatch = nmb
+
+    f = lambda stage, micro_batch_id: actions[2 * micro_batch_id * num_stage + stage]
+    b = lambda stage, micro_batch_id: actions[(2 * micro_batch_id + 1) * num_stage + num_stage - 1 - stage]
+
+    # action placement
+    for stage in range(num_stage):
+        for mid in range(num_microbatch):
+            f(stage, mid).device = stage % ndevice
+            print(f(stage, mid), f'stage={stage}, mid={mid}, device={stage % ndevice}')
+            b(stage, mid).device = stage % ndevice
+            print(b(stage, mid), f'stage={stage}, mid={mid}')
+
+    sequence = list()
+
+    # warmup:
+    for stage in range(num_stage):
+        for mid in range(num_stage-stage):
+            sequence.append(f(stage, mid))
+
+    # steady + cooldown:
+    for mid in range(num_microbatch):
+        # enqueue backward
+        for stage in range(num_stage-1, -1, -1):
+            sequence.append(b(stage, mid))
+        # enqueue forward
+        for stage in range(num_stage):
+            f_mid = mid + num_stage - stage
+            if f_mid >= num_microbatch:
+                continue
+            sequence.append(f(stage, f_mid))
+    print(sequence)
+    assert correct_check(sequence, actions, relations)
+    execplan = ExecutionPlan(sequence, ndevice)
+    execplan.draw(outfile='./pipeline-1f1b.png')
+
+
+def gpipe(actions, relations, nstage, ndevice, nmb):
+    num_stage = nstage
+    num_microbatch = nmb
+
+    f = lambda stage, micro_batch_id: actions[2 * micro_batch_id * num_stage + stage]
+    b = lambda stage, micro_batch_id: actions[(2 * micro_batch_id + 1) * num_stage + num_stage - 1 - stage]
+
+    # action placement
+    for stage in range(num_stage):
+        for mid in range(num_microbatch):
+            f(stage, mid).device = stage % ndevice
+            print(f(stage, mid), f'stage={stage}, mid={mid}, device={stage % ndevice}')
+            b(stage, mid).device = stage % ndevice
+            print(b(stage, mid), f'stage={stage}, mid={mid}')
+
+    sequence = list()
+
+    # warmup:
+    for stage in range(num_stage):
+        for mid in range(num_microbatch):
+            sequence.append(f(stage, mid))
+    
+    # backward
+    for stage in range(num_stage):
+        for mid in range(num_microbatch):
+            sequence.append(b(num_stage - 1 - stage, mid))
+
+    print(sequence)
+    # assert correct_check(sequence, actions, relations)
+    execplan = ExecutionPlan(sequence, ndevice)
+    execplan.draw(outfile='./gpipe.png')
 
 
 if __name__ == '__main__':
@@ -85,6 +158,8 @@ if __name__ == '__main__':
                         help='number of micro-batch')
     parser.add_argument('--ndev', type=int, default=4,
                         help='number of devices')
+    parser.add_argument('--max-time', type=int, default=100,
+                        help='maximal time. Will filter out plans that have larger time than this')
     args = parser.parse_args()
     
     forward = lambda data: data
@@ -92,5 +167,7 @@ if __name__ == '__main__':
 
     actions, relations = get_semantic(forward, backward, args.nstage, args.nmb)
 
-    #print_all_legal_sequence(actions, relations)
-    fixed_placement_sequence(actions, relations, args.ndev, forward, backward)
+    pipe_1f1b(actions, relations, args.nstage, args.ndev, args.nmb)
+    gpipe(actions, relations, args.nstage, args.ndev, args.nmb)
+
+    fixed_placement_sequence(actions, relations, args.ndev, args.max_time)
