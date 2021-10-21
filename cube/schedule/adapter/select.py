ac122104a6c51ff00e4eaf5a1dff414c0114f628
Zhiqi Lin <v-zhiql@microsoft.com>
2021-10-21 13:55:10 +0800

    add select for adapter

--- cube/schedule/adapter/select.py
+++ cube/schedule/adapter/select.py
@@ -1,9 +1,9 @@
-from typing import List
+from typing import List, Optional
 from enum import Enum
 import numpy as np
 
-from cube.ir.cten import IRCell, IRTensor
-from cube.graph.tensor import IRSubTensor, IRFullTensor
+from cube.ir.cten import IRCell
+from cube.graph.tensor import IRSubTensor, IndexMap
 
 
 class IRReshapeType(Enum):
@@ -25,25 +25,26 @@ class IRTensorReshape(IRCell):
         src_tensors has (multiple) tensors, dst_tensors is only one tensor.
         This will merge the sub_tensor and generate what it need
     """
-    def __init__(self, src_tensors: List[IRTensor], dst_tensors: List[IRTensor]):
+    def __init__(self, src_tensors: List[IRSubTensor], dst_tensors: List[IRSubTensor]):
 
         if len(src_tensors) != 1 and len(dst_tensors) != 1:
             raise ValueError("Expected at least one of tensors has length 1")
-        self._src_tensors = src_tensors
-        self._dst_tensors = dst_tensors
 
         self.ttype = None
 
-        self.select_indices = list()
-        self.merge_axis = None
+        self._select_indices: List[IndexMap] = list()
+        self._merge_axis = None
 
         if len(src_tensors) == 1:
             self.ttype = IRReshapeType.Select
             src_tensor = src_tensors[0]
+            if not isinstance(src_tensor, IRSubTensor):
+                raise TypeError(f"Expected IRSubTensor but got {type(src_tensor)}")
             # select
             for tensor in dst_tensors:
-                indices = tensor.common(src_tensor)
-                self.select_indices.append(indices)
+                indices = tensor.indices & src_tensor.indices
+                print(indices.get())
+                self._select_indices.append(indices)
         
         elif len(dst_tensors) == 1:
             self.ttype = IRReshapeType.Merge
@@ -54,34 +55,36 @@ class IRTensorReshape(IRCell):
             for src_tensor in src_tensors:
                 if isinstance(src_tensor, IRSubTensor):
                     for ndim, slicer in enumerate(src_tensor.indices.get()):
-                        indices[ndim].add(slicer)
-                elif isinstance(dst_tensor, IRFullTensor):
-                    for ndim, dim_len in enumerate(src_tensor.shape):
-                        slicer = slice(0, dim_len, 1)
-                        indices[ndim].add(slicer)
+                        indices[ndim].add((slicer.start, slicer.stop, slicer.step))
+                else:
+                    raise RuntimeError(
+                        f"Expected SubTensor but got {type(src_tensor)}"
+                    ) 
             # check if only one dim set has multiple slicer
             for dim, dim_indices in enumerate(indices):
                 if len(dim_indices) != 1:
-                    if self.merge_axis is not None:
+                    if self._merge_axis is not None:
                         raise NotImplementedError("Only support merge on one axis")
-                    self.merge_axis = dim
-            dim_indices = indices[self.merge_axis]
-            # check if they are overlapped
-            starts = np.array([slicer.start for slicer in dim_indices])
-            stops = np.array([slicer.stop for slicer in dim_indices])
-            steps = np.array([slicer.step for slicer in dim_indices])
-            sorted_idx = np.argsort(starts)
-            sorted_starts = starts[sorted_idx]
-            sorted_stops = stops[sorted_idx]
-            sorted_steps = steps[sorted_idx]
-            for last_stop, begin_start in zip(sorted_stops[:-1], sorted_starts[1:]):
-                if last_stop != begin_start:
-                    raise NotImplementedError(f"Concatenation fails due to axis {last_stop} != {begin_start}")
-            for step in sorted_steps:
-                if step != 1:
-                    raise NotImplementedError(f"Found a SubTensor step {step} != 1")
-            # re-order
-            dst_tensors = dst_tensors[sorted_idx]
+                    self._merge_axis = dim
+            # get merge axis
+            if self._merge_axis is not None:
+                dim_indices = indices[self._merge_axis]
+                # check if they are overlapped
+                starts = np.array([slicer[0] for slicer in dim_indices])
+                stops = np.array([slicer[1] for slicer in dim_indices])
+                steps = np.array([slicer[2] for slicer in dim_indices])
+                sorted_idx = np.argsort(starts)
+                sorted_starts = starts[sorted_idx]
+                sorted_stops = stops[sorted_idx]
+                sorted_steps = steps[sorted_idx]
+                for last_stop, begin_start in zip(sorted_stops[:-1], sorted_starts[1:]):
+                    if last_stop != begin_start:
+                        raise NotImplementedError(f"Concatenation fails due to axis {last_stop} != {begin_start}")
+                for step in sorted_steps:
+                    if step != 1:
+                        raise NotImplementedError(f"Found a SubTensor step {step} != 1")
+                # re-order
+                src_tensors = np.array(src_tensors)[sorted_idx]
 
         else:
             raise RuntimeError("Internal Error")
@@ -96,3 +99,20 @@ class IRTensorReshape(IRCell):
             self.set_input(idx, input)
         for idx, output in enumerate(dst_tensors):
             self.set_output(idx, output)
+
+    @property
+    def select_indices(self) -> List[IndexMap]:
+        return self._select_indices
+
+    @property
+    def merge_axis(self) -> Optional[int]:
+        return self._merge_axis
+
+    def is_identity(self):
+        """
+        Check if this transformation is a non-op
+        """
+        if len(self.inputs()) == 1 and len(self.outputs()) == 1:
+            if self.inputs(0) == self.outputs(0):
+                return True
+        return False
