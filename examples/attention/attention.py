b0e8a5570c3495a8b77394e0d3e3e076b18263d0
Zhiqi Lin <v-zhiql@microsoft.com>
2021-11-22 20:43:58 +0800

    fix command

--- examples/attention/attention.py
+++ examples/attention/attention.py
@@ -2,13 +2,13 @@
 example:
 
 python -m torch.distributed.launch \
-    --nproc_per_node=1 \
+    --nproc_per_node=4 \
     --nnodes=1 \
     --node_rank=0 \
     --master_addr=127.0.0.1 \
     --master_port=8004 \
     --use_env \
-    examples/attention.py
+    examples/attention/attention.py
 """
 
 import torch
