fix bugs on gradient accum

cube/runtime/executor.py
@@ -40,8 +40,12 @@ def backward(input_tensors: List[torch.Tensor], output_tensors, output_tensor_gr
         # print('backwarding... ')
         in_grads = torch.autograd.grad(output_tensors, inputs, output_tensor_grads)
         for idx, grad in zip(indices, in_grads):
-            if isinstance(input_tensors[idx], torch.nn.Parameter):
-                input_tensors[idx].grad = grad
+            tensor = input_tensors[idx]
+            if isinstance(tensor, torch.nn.Parameter):
+                if tensor.grad is not None:
+                    tensor.grad += grad
+                else:
+                    tensor.grad = grad
             grads[idx] = grad
 
     # if len(inputs) != 0: