aa38a03b02dc6ff1a196c22eef4daf067a9e93b7
Zhiqi Lin <v-zhiql@microsoft.com>
2021-11-28 18:47:12 +0800

    exp

---
+++ eval/swin.sh
@@ -0,0 +1,135 @@
+#!/usr/sh
+
+# ================== Megatron Policy Parallel ===============
+
+python -m torch.distributed.launch \
+    --nproc_per_node=2 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt.py --bs 8 \
+        --layer0 2 1 1 \
+        --layer1 2 1 1 \
+        --layer2 2 1 1 \
+        --layer3 2 1 1 \
+    > 2gpu_dp.txt
+
+python -m torch.distributed.launch \
+    --nproc_per_node=4 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt.py --bs 8 \
+        --layer0 4 1 1 \
+        --layer1 4 1 1 \
+        --layer2 4 1 1 \
+        --layer3 4 1 1 \
+    > 4gpu_dp.txt
+
+
+python -m torch.distributed.launch \
+    --nproc_per_node=8 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt.py --bs 8 \
+        --layer0 8 1 1 \
+        --layer1 8 1 1 \
+        --layer2 8 1 1 \
+        --layer3 8 1 1 \
+    > 8gpu_dp.txt
+
+# ================== Maximal Tensor Parallel ===============
+
+python -m torch.distributed.launch \
+    --nproc_per_node=2 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt.py --bs 8 \
+        --layer0 1 2 1 \
+        --layer1 1 2 1 \
+        --layer2 1 2 1 \
+        --layer3 1 2 1 \
+    > 2gpu_tp.txt
+
+python -m torch.distributed.launch \
+    --nproc_per_node=4 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt.py --bs 8 \
+        --layer0 1 2 1 \
+        --layer1 1 2 1 \
+        --layer2 1 2 1 \
+        --layer3 1 2 1 \
+    > 4gpu_tp.txt
+
+
+python -m torch.distributed.launch \
+    --nproc_per_node=8 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt.py --bs 8 \
+        --layer0 2 1 4 \
+        --layer1 2 1 4 \
+        --layer2 2 1 4 \
+        --layer3 2 1 4 \
+    > 8gpu_2dp4tp.txt
+
+# ================== Window + Tensor Parallel ===============
+
+python -m torch.distributed.launch \
+    --nproc_per_node=2 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt.py --bs 8 \
+        --layer0 1 2 1 \
+        --layer1 1 2 1 \
+        --layer2 1 1 2 \
+        --layer3 1 1 2 \
+    > 2gpu_2wp2tp.txt
+
+python -m torch.distributed.launch \
+    --nproc_per_node=4 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt.py --bs 8 \
+        --layer0 1 4 1 \
+        --layer1 1 4 1 \
+        --layer2 1 1 4 \
+        --layer3 1 1 4 \
+    > 2gpu_4wp4tp.txt
+
+python -m torch.distributed.launch \
+    --nproc_per_node=4 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt.py --bs 8 \
+        --layer0 1 8 1 \
+        --layer1 1 1 8 \
+        --layer2 1 1 4 \
+        --layer3 1 1 4 \
+    > 2gpu_8wp8tp.txt
