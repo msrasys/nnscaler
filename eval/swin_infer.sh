5bf2c74db9f95dd4856eb56e396f18992e12565b
Zhiqi Lin <v-zhiql@microsoft.com>
2021-11-29 12:35:18 +0800

    swin inference test

---
+++ eval/swin_infer.sh
@@ -0,0 +1,77 @@
+mkdir -p expinfer32
+
+# ================== Maximal Tensor Parallel ===============
+
+python -m torch.distributed.launch \
+    --nproc_per_node=2 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt_infer.py --bs 1 \
+        --layer0 1 1 2 \
+        --layer1 1 1 2 \
+        --layer2 1 1 2 \
+        --layer3 1 1 2 \
+    > expinfer32/2gpu_tp.txt
+
+python -m torch.distributed.launch \
+    --nproc_per_node=4 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt_infer.py --bs 1 \
+        --layer0 1 1 4 \
+        --layer1 1 1 4 \
+        --layer2 1 1 4 \
+        --layer3 1 1 4 \
+    > expinfer32/4gpu_tp.txt
+
+
+# ================== Window + Tensor Parallel ===============
+
+python -m torch.distributed.launch \
+    --nproc_per_node=2 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt_infer.py --bs 1 \
+        --layer0 1 2 1 \
+        --layer1 1 2 1 \
+        --layer2 1 2 1 \
+        --layer3 1 1 2 \
+    > expinfer32/2gpu_2wp2tp.txt
+
+python -m torch.distributed.launch \
+    --nproc_per_node=4 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt_infer.py --bs 1 \
+        --layer0 1 4 1 \
+        --layer1 1 4 1 \
+        --layer2 1 4 1 \
+        --layer3 1 1 4 \
+    > expinfer32/4gpu_4wp4tp.txt
+
+python -m torch.distributed.launch \
+    --nproc_per_node=8 \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=127.0.0.1 \
+    --master_port=8004 \
+    --use_env \
+    examples/swin/swin_dwt_infer.py --bs 1 \
+        --layer0 1 8 1 \
+        --layer1 1 1 8 \
+        --layer2 1 1 8 \
+        --layer3 1 1 8 \
+    > expinfer32/8gpu_8wp8tp.txt
+
