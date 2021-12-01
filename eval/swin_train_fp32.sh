9aa8cc82fe10a54bc11bae20742df6b182c74ab9
Zhiqi Lin <v-zhiql@microsoft.com>
2021-12-01 13:40:33 +0000

    eval scripts

--- eval/swin_train_fp32.sh
+++ eval/swin_train_fp32.sh
@@ -1,8 +1,22 @@
 bs=$1
 
-logfile=exptrain_782M_bs${bs}_fp32
+logfile=exptrain_782M_bs${bs}_fp32_384
 
 mkdir -p ${logfile}
+
+# python -m torch.distributed.launch \
+#     --nproc_per_node=1 \
+#     --nnodes=1 \
+#     --node_rank=0 \
+#     --master_addr=127.0.0.1 \
+#     --master_port=8004 \
+#     --use_env \
+#     examples/swin/swin_dwt.py --bs ${bs} \
+#         --layer0 1 1 1 \
+#         --layer1 1 1 1 \
+#         --layer2 1 1 1 \
+#         --layer3 1 1 1 \
+#     > ${logfile}/single.txt
 
 # ================== Megatron Policy Parallel ===============
 
@@ -18,7 +32,7 @@ python -m torch.distributed.launch \
         --layer1 1 1 2 \
         --layer2 1 1 2 \
         --layer3 1 1 2 \
-    > ${logfile}/2gpu_maxdp.txt
+    > ${logfile}/2gpu_maxdp2tp.txt
 
 python -m torch.distributed.launch \
     --nproc_per_node=4 \
@@ -32,7 +46,7 @@ python -m torch.distributed.launch \
         --layer1 2 1 2 \
         --layer2 2 1 2 \
         --layer3 2 1 2 \
-    > ${logfile}/4gpu_maxdp.txt
+    > ${logfile}/4gpu_maxdp2tp.txt
 
 
 python -m torch.distributed.launch \
@@ -47,7 +61,7 @@ python -m torch.distributed.launch \
         --layer1 4 1 2 \
         --layer2 4 1 2 \
         --layer3 4 1 2 \
-    > ${logfile}/8gpu_maxdp.txt
+    > ${logfile}/8gpu_maxdp2tp.txt
 
 # ================== Maximal Tensor Parallel ===============
 
@@ -181,3 +195,49 @@ python -m torch.distributed.launch \
         --layer2 1 8 \
         --layer3 1 8 \
     > ${logfile}/8gpu_dt_8dp8tp.txt
+
+
+# ================== Pure Data Parallel =============
+
+# python -m torch.distributed.launch \
+#     --nproc_per_node=2 \
+#     --nnodes=1 \
+#     --node_rank=0 \
+#     --master_addr=127.0.0.1 \
+#     --master_port=8004 \
+#     --use_env \
+#     examples/swin/swin_dwt.py --bs ${bs} \
+#         --layer0 2 1 1 \
+#         --layer1 2 1 1 \
+#         --layer2 2 1 1 \
+#         --layer3 2 1 1 \
+#     > ${logfile}/2gpu_maxdp.txt
+# 
+# 
+# python -m torch.distributed.launch \
+#     --nproc_per_node=4 \
+#     --nnodes=1 \
+#     --node_rank=0 \
+#     --master_addr=127.0.0.1 \
+#     --master_port=8004 \
+#     --use_env \
+#     examples/swin/swin_dwt.py --bs ${bs} \
+#         --layer0 4 1 1 \
+#         --layer1 4 1 1 \
+#         --layer2 4 1 1 \
+#         --layer3 4 1 1 \
+#     > ${logfile}/4gpu_maxdp.txt
+# 
+# python -m torch.distributed.launch \
+#     --nproc_per_node=8 \
+#     --nnodes=1 \
+#     --node_rank=0 \
+#     --master_addr=127.0.0.1 \
+#     --master_port=8004 \
+#     --use_env \
+#     examples/swin/swin_dwt.py --bs ${bs} \
+#         --layer0 8 1 1 \
+#         --layer1 8 1 1 \
+#         --layer2 8 1 1 \
+#         --layer3 8 1 1 \
+#     > ${logfile}/8gpu_maxdp.txt
