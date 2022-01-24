0d8900a1d197c30e47d7d7cefdd91a5489e80ac8
Zhiqi Lin <v-zhiql@microsoft.com>
2022-01-24 07:05:52 +0000

    PAS with replicate

--- examples/poisson/policy/naive.py
+++ examples/poisson/policy/naive.py
@@ -2,6 +2,8 @@ from cube.graph import IRGraph
 
 def PAS(graph: IRGraph, resource):
     for node in graph.nodes():
-        graph.assign(node, 0)
+        sub_nodes = graph.replicate(node, times=resource.ngpus)
+        for idx, sub_node in enumerate(sub_nodes):
+            graph.assign(sub_node, idx)
     print(graph.extra_repr())
     return graph
