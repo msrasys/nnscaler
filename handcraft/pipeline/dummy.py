98b1e12b2e914879fcd71d271928beca1ac17a21
Zhiqi Lin <v-zhiql@microsoft.com>
2022-03-04 16:32:15 +0800

    update with embedding layer

--- handcraft/pipeline/dummy.py
+++ handcraft/pipeline/dummy.py
@@ -16,45 +16,96 @@ import torch.nn.functional as F
 import cube
 from cube.profiler import CudaTimer
 from cube.profiler.timer import print_each_rank
+from cube.profiler.memory import memory_summary
 from cube.runtime.device import DeviceGroup
-from cube.runtime.syndata import SynDataLoader
+from cube.runtime.syndata import SynDataLoader, SynTextDataLoader
 
 from handcraft.pipeline.schedule import schedule_tp_1f1b, schedule_naive
 import argparse
 
+"""
+Stage0:
+    Embedding [M, 1], [N, E] -> [M, E]
+    Linear    [M, E], [E, E] -> [M, E]
+
+Stage Else:
+    Linear [M, E], [E, E] -> [M, E]
+        
+Condition: N > 8M - E
+"""
+
+class ReduceEmbed(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, input):
+        torch.distributed.all_reduce(input)
+        return input
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output
+
+
 
 class DummyModel(torch.nn.Module):
 
-    def __init__(self, dim: int, bs: int, stage_id: int, sharding=False):
+    def __init__(self, M: int, N: int, E: int, stage_id: int, sharding=False):
 
         super().__init__()
-        self.bs = bs
-        self.dim = dim
+        self.M = M
+        self.N = N
+        self.E = E
         self.is_last_stage = stage_id == DeviceGroup().world_size - 1
-        if sharding:
-            chunk_num = torch.distributed.get_world_size()
-            self.weight = torch.nn.Parameter(torch.zeros((dim // chunk_num, dim)))
+        self.is_first_stage = stage_id == 0
+        self.sharding = sharding
+        # first stage
+        chunk_num = torch.distributed.get_world_size() if sharding else 1
+        if self.is_first_stage:
+            self.vocab_start_index = N // chunk_num * stage_id
+            self.vocab_end_index = N // chunk_num * (stage_id + 1)
+            self.embed_weight = torch.nn.Parameter(torch.zeros((N // chunk_num, E)))
+            self.fc_weight = torch.nn.Parameter(torch.zeros((E // chunk_num, E)))
         else:
-            self.weight = torch.nn.Parameter(torch.zeros((dim, dim)))
+            self.fc_weight = torch.nn.Parameter(torch.zeros((E // chunk_num, E)))
 
     def input_shape(self):
-        return (self.bs, self.dim, self.dim)
+        if self.is_first_stage:
+            return (self.M,)
+        else:
+            return (self.M, self.E)
 
     def output_shape(self):
-        return (1,) if self.is_last_stage else (self.bs, self.dim, self.dim)
+        if self.is_last_stage:
+            return (1,)
+        else:
+            return (self.M, self.E)
 
     def input_dtype(self):
-        return torch.float32
+        if self.is_first_stage:
+            return torch.int64
+        else:
+            return torch.float32
 
     def output_dtype(self):
         return torch.float32
 
-    def forward(self, input):
-        output = F.linear(input, self.weight)
+    def forward(self, input: torch.Tensor):
+        if self.is_first_stage:
+            if self.sharding:
+                mask = (input < self.vocab_start_index) | \
+                        (input >= self.vocab_end_index)
+                input = input.clone() - self.vocab_start_index
+                input[mask] = 0
+                input = F.embedding(input, self.embed_weight)
+                input = ReduceEmbed.apply(input)
+            else:
+                input = F.embedding(input, self.embed_weight)
+
+        output = F.linear(input, self.fc_weight)
+
         if self.is_last_stage:
             output = torch.sum(output)
         return output
-        
 
 
 if __name__ == '__main__':
@@ -66,31 +117,34 @@ if __name__ == '__main__':
                         help='use tensor parallel 1f1b')
     parser.add_argument('--nmb', type=int, default=4,
                         help='num of micro batch')
+    parser.add_argument('--M', type=int, default=4096,
+                        help='M dimension length = sequence length')
+    parser.add_argument('--N', type=int, default=50257,
+                        help='word number')
+    parser.add_argument('--E', type=int, default=2048,
+                        help='E dimension length = hidden dimension length')
     args = parser.parse_args()
     assert args.use_naive ^ args.use_tp1f1b, "Specify (only) 1 way pipeline"
+    print(args)
 
     cube.init()
     rank = DeviceGroup().rank
 
-    dim = 2048
-    mbs = 8
-    gbs = mbs * args.nmb
-
     # tp 1f1b
     if args.use_tp1f1b:
-        first_stage_model = DummyModel(dim, mbs, 0, sharding=True).cuda()
+        first_stage_model = DummyModel(args.M, args.N, args.E, 0, sharding=True).cuda()
         if rank == 0:
             model = None
         else:
-            model = DummyModel(dim, mbs, rank, sharding=False).cuda()
+            model = DummyModel(args.M, args.N, args.E, rank, sharding=False).cuda()
 
     if args.use_naive:
         # naive pipleline
-        model = DummyModel(dim, mbs, rank, sharding=False).cuda()
+        model = DummyModel(args.M, args.N, args.E, rank, sharding=False).cuda()
 
-    dataloader = SynDataLoader(
-        shapes=([mbs, dim, dim],),
-        dtypes=(torch.float32, ),
+    dataloader = SynTextDataLoader(
+        shapes=([args.M],),
+        dtypes=(torch.int64, ),
         batch_dims=(0,),
         length=128000
     )
@@ -113,3 +167,4 @@ if __name__ == '__main__':
     
     print_each_rank('e2e time (ms) per iteration: {} ms'.format(
           CudaTimer().duration(iter_num-20, field_name='e2e')))
+    memory_summary()
