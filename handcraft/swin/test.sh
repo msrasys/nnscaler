9dbd10f39d9a910de7c9921c5401f216168e76ea
Zhiqi Lin <v-zhiql@microsoft.com>
2022-03-30 02:10:14 +0000

    add test script

--- handcraft/swin/test.sh
+++ handcraft/swin/test.sh
@@ -4,159 +4,65 @@ evaldir=eval/swin-coshard
 mkdir -p ${evaldir}
 
 
-layers=18
-dim=192
-heads=6
 img_size=1536
 window_size=48
-coshard=6
-gpus=4
-
-OMP_NUM_THREADS=4 torchrun \
-  --nproc_per_node=${gpus} \
-  --nnodes=1 \
-  handcraft/swin/train.py \
-    --layers ${layers} --dim ${dim} --heads ${heads} \
-    --img-size ${img_size} --window-size ${window_size} \
-    --pp-size ${gpus} --tp-size 1 --dp-size 1  \
-    --bs 256 --micro-bs 1 --coshard ${coshard} --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-pp${gpus}-coshard${coshard}.txt
 
 
-OMP_NUM_THREADS=4 torchrun \
-  --nproc_per_node=${gpus} \
-  --nnodes=1 \
-  handcraft/swin/train.py \
-    --layers ${layers} --dim ${dim} --heads ${heads} \
-    --img-size ${img_size} --window-size ${window_size} \
-    --pp-size 1 --tp-size ${gpus} --dp-size 1  \
-    --bs 256 --micro-bs 1 --coshard 1 --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-tp${gpus}-coshard1.txt
+test()
+{
+  layers=$1
+  dim=$2
+  heads=$3
+  coshard=$4
+  gpus=$5
 
+  echo "testing ${gpus}-dev: PP-Coshard${coshard}: L${layers}E${dim}H${heads}"
+  echo "OMP_NUM_THREADS=4 torchrun \
+    --nproc_per_node=${gpus} \
+    --nnodes=1 \
+    handcraft/swin/train.py \
+      --layers ${layers} --dim ${dim} --heads ${heads} \
+      --img-size ${img_size} --window-size ${window_size} \
+      --pp-size ${gpus} --tp-size 1 --dp-size 1  \
+      --bs 256 --micro-bs 1 --coshard ${coshard} --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-pp${gpus}-coshard${coshard}.txt"
 
-OMP_NUM_THREADS=4 torchrun \
-  --nproc_per_node=${gpus} \
-  --nnodes=1 \
-  handcraft/swin/train.py \
-    --layers ${layers} --dim ${dim} --heads ${heads} \
-    --img-size ${img_size} --window-size ${window_size} \
-    --pp-size ${gpus} --tp-size 1 --dp-size 1  \
-    --bs 256 --micro-bs 1 --coshard 1 --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-pp${gpus}-coshard1.txt
+  echo "testing ${gpus}-dev: TP-Coshard1: L${layers}E${dim}H${heads}"
+  echo "OMP_NUM_THREADS=4 torchrun \
+    --nproc_per_node=${gpus} \
+    --nnodes=1 \
+    handcraft/swin/train.py \
+      --layers ${layers} --dim ${dim} --heads ${heads} \
+      --img-size ${img_size} --window-size ${window_size} \
+      --pp-size 1 --tp-size ${gpus} --dp-size 1  \
+      --bs 256 --micro-bs 1 --coshard 1 --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-tp${gpus}-coshard1.txt"
 
+  echo "testing ${gpus}-dev: PP-Coshard1: L${layers}E${dim}H${heads}"
+  echo "OMP_NUM_THREADS=4 torchrun \
+    --nproc_per_node=${gpus} \
+    --nnodes=1 \
+    handcraft/swin/train.py \
+      --layers ${layers} --dim ${dim} --heads ${heads} \
+      --img-size ${img_size} --window-size ${window_size} \
+      --pp-size ${gpus} --tp-size 1 --dp-size 1  \
+      --bs 256 --micro-bs 1 --coshard 1 --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-pp${gpus}-coshard1.txt"
 
+  killall python
+  sleep 5
+  killall python
+}
 
-layers=26
-dim=384
-heads=12
-img_size=1536
-window_size=48
-coshard=12
-gpus=4
+# test Layers Dim Heads Coshard GPUs
+test 18 256 8  8  4
+test 18 512 16 16 4
+test 18 768 24 24 4
 
-OMP_NUM_THREADS=4 torchrun \
-  --nproc_per_node=${gpus} \
-  --nnodes=1 \
-  handcraft/swin/train.py \
-    --layers ${layers} --dim ${dim} --heads ${heads} \
-    --img-size ${img_size} --window-size ${window_size} \
-    --pp-size ${gpus} --tp-size 1 --dp-size 1  \
-    --bs 256 --micro-bs 1 --coshard ${coshard} --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-pp${gpus}-coshard${coshard}.txt
+test 26 512 16 16 4
+test 26 768 24 24 4
+test 26 1024 32 32 4
 
-
-OMP_NUM_THREADS=4 torchrun \
-  --nproc_per_node=${gpus} \
-  --nnodes=1 \
-  handcraft/swin/train.py \
-    --layers ${layers} --dim ${dim} --heads ${heads} \
-    --img-size ${img_size} --window-size ${window_size} \
-    --pp-size 1 --tp-size ${gpus} --dp-size 1  \
-    --bs 256 --micro-bs 1 --coshard 1 --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-tp${gpus}-coshard1.txt
-
-
-OMP_NUM_THREADS=4 torchrun \
-  --nproc_per_node=${gpus} \
-  --nnodes=1 \
-  handcraft/swin/train.py \
-    --layers ${layers} --dim ${dim} --heads ${heads} \
-    --img-size ${img_size} --window-size ${window_size} \
-    --pp-size ${gpus} --tp-size 1 --dp-size 1  \
-    --bs 256 --micro-bs 1 --coshard 1 --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-pp${gpus}-coshard1.txt
-
-
-
-layers=42
-dim=512
-heads=16
-img_size=1536
-window_size=48
-coshard=16
-gpus=4
-
-OMP_NUM_THREADS=4 torchrun \
-  --nproc_per_node=${gpus} \
-  --nnodes=1 \
-  handcraft/swin/train.py \
-    --layers ${layers} --dim ${dim} --heads ${heads} \
-    --img-size ${img_size} --window-size ${window_size} \
-    --pp-size ${gpus} --tp-size 1 --dp-size 1  \
-    --bs 256 --micro-bs 1 --coshard ${coshard} --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-pp${gpus}-coshard${coshard}.txt
-
-
-OMP_NUM_THREADS=4 torchrun \
-  --nproc_per_node=${gpus} \
-  --nnodes=1 \
-  handcraft/swin/train.py \
-    --layers ${layers} --dim ${dim} --heads ${heads} \
-    --img-size ${img_size} --window-size ${window_size} \
-    --pp-size 1 --tp-size ${gpus} --dp-size 1  \
-    --bs 256 --micro-bs 1 --coshard 1 --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-tp${gpus}-coshard1.txt
-
-
-OMP_NUM_THREADS=4 torchrun \
-  --nproc_per_node=${gpus} \
-  --nnodes=1 \
-  handcraft/swin/train.py \
-    --layers ${layers} --dim ${dim} --heads ${heads} \
-    --img-size ${img_size} --window-size ${window_size} \
-    --pp-size ${gpus} --tp-size 1 --dp-size 1  \
-    --bs 256 --micro-bs 1 --coshard 1 --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-pp${gpus}-coshard1.txt
-
-
-
-layers=50
-dim=768
-heads=24
-img_size=1536
-window_size=48
-coshard=16
-gpus=8
-
-OMP_NUM_THREADS=4 torchrun \
-  --nproc_per_node=${gpus} \
-  --nnodes=1 \
-  handcraft/swin/train.py \
-    --layers ${layers} --dim ${dim} --heads ${heads} \
-    --img-size ${img_size} --window-size ${window_size} \
-    --pp-size ${gpus} --tp-size 1 --dp-size 1  \
-    --bs 256 --micro-bs 1 --coshard ${coshard} --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-pp${gpus}-coshard${coshard}.txt
-
-
-OMP_NUM_THREADS=4 torchrun \
-  --nproc_per_node=${gpus} \
-  --nnodes=1 \
-  handcraft/swin/train.py \
-    --layers ${layers} --dim ${dim} --heads ${heads} \
-    --img-size ${img_size} --window-size ${window_size} \
-    --pp-size 1 --tp-size ${gpus} --dp-size 1  \
-    --bs 256 --micro-bs 1 --coshard 1 --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-tp${gpus}-coshard1.txt
-
-
-OMP_NUM_THREADS=4 torchrun \
-  --nproc_per_node=${gpus} \
-  --nnodes=1 \
-  handcraft/swin/train.py \
-    --layers ${layers} --dim ${dim} --heads ${heads} \
-    --img-size ${img_size} --window-size ${window_size} \
-    --pp-size ${gpus} --tp-size 1 --dp-size 1  \
-    --bs 256 --micro-bs 1 --coshard 1 --fp16 > ${evaldir}/${gpus}dev-L${layers}E${dim}H${heads}-${img_size}-pp${gpus}-coshard1.txt
-
+test 34 256 8  8  8
+test 34 512 16 16 8
+test 34 768 24 24 8
+test 34 1024 32 32 8
 
 python scripts/keep.py --gpus 8
