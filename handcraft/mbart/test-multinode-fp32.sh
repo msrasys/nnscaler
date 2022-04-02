9b94a2228f142f9d697f287bfc7151d07bffea55
Zhiqi Lin <v-zhiql@microsoft.com>
2022-04-02 15:28:39 +0000

    add multi-node test

---
+++ handcraft/mbart/test-multinode-fp32.sh
@@ -0,0 +1,172 @@
+evaldir=eval/mbart-fp32-v100-32gb
+mkdir -p ${evaldir}
+
+bs=256
+
+test_mix_tp_1f1b()
+{
+    layers=$1
+    hidden=$2
+    heads=$3
+    gpus=$4
+    echo "testing ${gpus}-dev mixture-1f1b: L${layers}E${hidden}H${heads}"
+    OMP_NUM_THREADS=4 torchrun \
+      --nproc_per_node=8 \
+      --nnodes=2 \
+      --node_rank=${NODE_RANK} \
+      --master_addr="${MASTER_IP}" \
+      --master_port=${MASTER_PORT} \
+      handcraft/mbart/train.py \
+        --layers ${layers} --hidden-size ${hidden} --heads ${heads} \
+        --bs ${bs} --micro-bs 1 \
+        --pp-size ${gpus} --tp-size 1 \
+        --schedule tp1f1b > ${evaldir}/${gpus}dev-L${layers}E${hidden}H${heads}-tp1f1b.txt
+    sleep 5
+    killall python
+    sleep 5
+    killall python
+}
+
+test_tp()
+{
+    layers=$1
+    hidden=$2
+    heads=$3
+    gpus=$4
+    echo "testing ${gpus}-dev pure tp: L${layers}E${hidden}H${heads}"
+    OMP_NUM_THREADS=4 torchrun \
+      --nproc_per_node=8 \
+      --nnodes=2 \
+      --node_rank=${NODE_RANK} \
+      --master_addr="${MASTER_IP}" \
+      --master_port=${MASTER_PORT} \
+      handcraft/mbart/train.py \
+        --layers ${layers} --hidden-size ${hidden} --heads ${heads} \
+        --bs ${bs} --micro-bs 1 \
+        --pp-size 1 --tp-size ${gpus} \
+        --schedule 1f1b > ${evaldir}/${gpus}dev-L${layers}E${hidden}H${heads}-tp.txt
+    sleep 5
+    killall python
+    sleep 5
+    killall python
+}
+
+test_pp()
+{
+    layers=$1
+    hidden=$2
+    heads=$3
+    gpus=$4
+    echo "testing ${gpus}-dev pure pp: L${layers}E${hidden}H${heads}"
+    OMP_NUM_THREADS=4 torchrun \
+      --nproc_per_node=8 \
+      --nnodes=2 \
+      --node_rank=${NODE_RANK} \
+      --master_addr="${MASTER_IP}" \
+      --master_port=${MASTER_PORT} \
+      handcraft/mbart/train.py \
+        --layers ${layers} --hidden-size ${hidden} --heads ${heads} \
+        --bs ${bs} --micro-bs 1 \
+        --pp-size ${gpus} --tp-size 1 \
+        --schedule 1f1b > ${evaldir}/${gpus}dev-L${layers}E${hidden}H${heads}-pp.txt
+    sleep 5
+    killall python
+    sleep 5
+    killall python
+}
+
+test_pp_swap()
+{
+    layers=$1
+    hidden=$2
+    heads=$3
+    gpus=$4
+    echo "testing ${gpus}-dev pure pp swap: L${layers}E${hidden}H${heads}"
+    OMP_NUM_THREADS=4 torchrun \
+      --nproc_per_node=8 \
+      --nnodes=2 \
+      --node_rank=${NODE_RANK} \
+      --master_addr="${MASTER_IP}" \
+      --master_port=${MASTER_PORT} \
+      handcraft/mbart/train.py \
+        --layers ${layers} --hidden-size ${hidden} --heads ${heads} \
+        --bs ${bs} --micro-bs 1 \
+        --pp-size ${gpus} --tp-size 1 \
+        --schedule 1f1b --use-swap > ${evaldir}/${gpus}dev-L${layers}E${hidden}H${heads}-pp-swap.txt
+    sleep 5
+    killall python
+    sleep 5
+    killall python
+}
+
+test_hybrid_tp_pp()
+{
+    layers=$1
+    hidden=$2
+    heads=$3
+    gpus=$4
+
+    if [ ${gpus} == 16 ]
+    then
+        echo "testing ${gpus}-dev tp:pp=2:2 | L${layers}E${hidden}H${heads}"
+        OMP_NUM_THREADS=4 torchrun \
+          --nproc_per_node=8 \
+          --nnodes=2 \
+          --node_rank=${NODE_RANK} \
+          --master_addr="${MASTER_IP}" \
+          --master_port=${MASTER_PORT} \
+          handcraft/mbart/mbart_hybrid.py \
+            --layers ${layers} --hidden-size ${hidden} --heads ${heads} \
+            --bs ${bs} --micro-bs 1 \
+            --pp-size 2 --tp-size 8 \
+            --schedule 1f1b > ${evaldir}/${gpus}dev-L${layers}E${hidden}H${heads}-tp8pp2.txt
+        sleep 5
+        killall python
+        sleep 5
+        killall python
+
+        echo "testing ${gpus}-dev tp:pp=2:2 | L${layers}E${hidden}H${heads}"
+        OMP_NUM_THREADS=4 torchrun \
+          --nproc_per_node=8 \
+          --nnodes=2 \
+          --node_rank=${NODE_RANK} \
+          --master_addr="${MASTER_IP}" \
+          --master_port=${MASTER_PORT} \
+          handcraft/mbart/mbart_hybrid.py \
+            --layers ${layers} --hidden-size ${hidden} --heads ${heads} \
+            --bs ${bs} --micro-bs 1 \
+            --pp-size 4 --tp-size 4 \
+            --schedule 1f1b > ${evaldir}/${gpus}dev-L${layers}E${hidden}H${heads}-tp4pp4.txt
+        sleep 5
+        killall python
+        sleep 5
+        killall python
+
+        echo "testing ${gpus}-dev tp:pp=2:2 | L${layers}E${hidden}H${heads}"
+        OMP_NUM_THREADS=4 torchrun \
+          --nproc_per_node=8 \
+          --nnodes=2 \
+          --node_rank=${NODE_RANK} \
+          --master_addr="${MASTER_IP}" \
+          --master_port=${MASTER_PORT} \
+          handcraft/mbart/mbart_hybrid.py \
+            --layers ${layers} --hidden-size ${hidden} --heads ${heads} \
+            --bs ${bs} --micro-bs 1 \
+            --pp-size 8 --tp-size 2 \
+            --schedule 1f1b > ${evaldir}/${gpus}dev-L${layers}E${hidden}H${heads}-tp8pp2.txt
+        sleep 5
+        killall python
+        sleep 5
+        killall python
+    fi
+}
+
+
+# =================================================
+# selected experiments
+# =================================================
+test_mix_tp_1f1b  32 5120 40 16
+test_tp           32 5120 40 16
+# test_hybrid_tp_pp 32 5120 40 16  # --> OOM
+
+python scripts/keep.py --gpus 8
