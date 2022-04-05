672f154ea39b1557c7fad87c5f61a80692ef9c01
Zhiqi Lin <v-zhiql@microsoft.com>
2022-04-05 07:50:34 +0000

    add test scripts

--- handcraft/mbart/test-2node-fp32.sh
+++ handcraft/mbart/test-2node-fp32.sh
@@ -108,14 +108,14 @@ test_hybrid_tp_pp()
 
     if [ ${gpus} == 16 ]
     then
-        echo "testing ${gpus}-dev tp:pp=2:2 | L${layers}E${hidden}H${heads}"
+        echo "testing ${gpus}-dev tp:pp=8:2 | L${layers}E${hidden}H${heads}"
         OMP_NUM_THREADS=4 torchrun \
           --nproc_per_node=8 \
           --nnodes=2 \
           --node_rank=${NODE_RANK} \
           --master_addr="${MASTER_IP}" \
           --master_port=${MASTER_PORT} \
-          handcraft/mbart/mbart_hybrid.py \
+          handcraft/mbart/train.py \
             --layers ${layers} --hidden-size ${hidden} --heads ${heads} \
             --bs ${bs} --micro-bs 1 \
             --pp-size 2 --tp-size 8 \
@@ -125,39 +125,39 @@ test_hybrid_tp_pp()
         sleep 5
         killall python
 
-        echo "testing ${gpus}-dev tp:pp=2:2 | L${layers}E${hidden}H${heads}"
-        OMP_NUM_THREADS=4 torchrun \
-          --nproc_per_node=8 \
-          --nnodes=2 \
-          --node_rank=${NODE_RANK} \
-          --master_addr="${MASTER_IP}" \
-          --master_port=${MASTER_PORT} \
-          handcraft/mbart/mbart_hybrid.py \
-            --layers ${layers} --hidden-size ${hidden} --heads ${heads} \
-            --bs ${bs} --micro-bs 1 \
-            --pp-size 4 --tp-size 4 \
-            --schedule 1f1b > ${evaldir}/${gpus}dev-L${layers}E${hidden}H${heads}-tp4pp4.txt
-        sleep 5
-        killall python
-        sleep 5
-        killall python
-
-        echo "testing ${gpus}-dev tp:pp=2:2 | L${layers}E${hidden}H${heads}"
-        OMP_NUM_THREADS=4 torchrun \
-          --nproc_per_node=8 \
-          --nnodes=2 \
-          --node_rank=${NODE_RANK} \
-          --master_addr="${MASTER_IP}" \
-          --master_port=${MASTER_PORT} \
-          handcraft/mbart/mbart_hybrid.py \
-            --layers ${layers} --hidden-size ${hidden} --heads ${heads} \
-            --bs ${bs} --micro-bs 1 \
-            --pp-size 8 --tp-size 2 \
-            --schedule 1f1b > ${evaldir}/${gpus}dev-L${layers}E${hidden}H${heads}-tp8pp2.txt
-        sleep 5
-        killall python
-        sleep 5
-        killall python
+        # echo "testing ${gpus}-dev tp:pp=4:4 | L${layers}E${hidden}H${heads}"
+        # OMP_NUM_THREADS=4 torchrun \
+        #   --nproc_per_node=8 \
+        #   --nnodes=2 \
+        #   --node_rank=${NODE_RANK} \
+        #   --master_addr="${MASTER_IP}" \
+        #   --master_port=${MASTER_PORT} \
+        #   handcraft/mbart/train.py \
+        #     --layers ${layers} --hidden-size ${hidden} --heads ${heads} \
+        #     --bs ${bs} --micro-bs 1 \
+        #     --pp-size 4 --tp-size 4 \
+        #     --schedule 1f1b > ${evaldir}/${gpus}dev-L${layers}E${hidden}H${heads}-tp4pp4.txt
+        # sleep 5
+        # killall python
+        # sleep 5
+        # killall python
+        # 
+        # echo "testing ${gpus}-dev tp:pp=2:8 | L${layers}E${hidden}H${heads}"
+        # OMP_NUM_THREADS=4 torchrun \
+        #   --nproc_per_node=8 \
+        #   --nnodes=2 \
+        #   --node_rank=${NODE_RANK} \
+        #   --master_addr="${MASTER_IP}" \
+        #   --master_port=${MASTER_PORT} \
+        #   handcraft/mbart/train.py \
+        #     --layers ${layers} --hidden-size ${hidden} --heads ${heads} \
+        #     --bs ${bs} --micro-bs 1 \
+        #     --pp-size 8 --tp-size 2 \
+        #     --schedule 1f1b > ${evaldir}/${gpus}dev-L${layers}E${hidden}H${heads}-tp8pp2.txt
+        # sleep 5
+        # killall python
+        # sleep 5
+        # killall python
     fi
 }
 
@@ -167,12 +167,13 @@ test_hybrid_tp_pp()
 # =================================================
 
 # strong scalability test
-test_mix_tp_1f1b  16 3072 24 16
-test_tp           16 3072 24 16
+# test_mix_tp_1f1b  16 3072 24 16
+# test_tp           16 3072 24 16
 
 # model scaling test
-test_mix_tp_1f1b  32 5120 40 16
-test_tp           32 5120 40 16
-# test_hybrid_tp_pp 32 5120 40 16  # --> OOM
+test_mix_tp_1f1b  36 5120 32 16
+test_tp           36 5120 32 16
+# test_hybrid_tp_pp 40 5120 32 16  # --> OOM
+# test_hybrid_tp_pp 36 5120 32 16  # --> OOM
 
 python scripts/keep.py --gpus 8
