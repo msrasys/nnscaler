04ca6ab34cb4087b0a7307e8f598d713d9e7648e
yizhu1 <yizhu1@microsoft.com>
2022-09-16 02:49:25 -0700

    fix tp bug

--- examples/nlp/palm/policy/spmd.py
+++ examples/nlp/palm/policy/spmd.py
@@ -43,14 +43,6 @@ def PASMegatron(graph: IRGraph, resource):
     tp_size = resource.ngpus
     tp_devs = list(range(tp_size))
 
-    for node in graph.nodes():
-        if isinstance(node, IRDataOperation):
-            algo = node.algorithms('data')
-            sub_nodes = graph.partition(node, algo, num=resource.ngpus)
-            for idx, sub_node in enumerate(sub_nodes):
-                graph.assign(sub_node, idx)
-            batch_dim = node.get_batch_dims()[0]
-
     def _tp(graph: IRGraph, node: IRFwOperation, devs: List[int], idx: int, dim: int):
         algo = node.algorithms('dim')
         sub_nodes = graph.partition(node, algo, idx=idx, dim=dim, num=len(devs))
@@ -64,6 +56,11 @@ def PASMegatron(graph: IRGraph, resource):
         for dev_id, sub_node in zip(devs, sub_nodes):
             graph.assign(sub_node, dev_id)
         return sub_nodes
+
+    for node in graph.nodes():
+        if isinstance(node, IRDataOperation):
+            _replica(graph, node, tp_devs)
+            batch_dim = node.get_batch_dims()[0]
     
     for node in graph.nodes():
         if isinstance(node, IRFwOperation):
