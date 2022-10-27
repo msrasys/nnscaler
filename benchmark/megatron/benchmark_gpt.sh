291f275b902f099ec110d08e0409f339028de6c4
Zhiqi Lin <v-zhiql@microsoft.com>
2022-10-27 12:46:31 +0000

    benchmark gpt script update

--- benchmark/megatron/benchmark_gpt.sh
+++ benchmark/megatron/benchmark_gpt.sh
@@ -1,20 +1,26 @@
-
-# get megatron
+# setup megatron
 # git clone https://github.com/NVIDIA/Megatron-LM.git
+# pip install regex
+
+# setup apex
+# git clone https://github.com/NVIDIA/apex
+# cd apex
+# pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" .
+# cd ..
 
 cp pretrain_gpt_synthetic.py ./Megatron-LM/
 
-GPUS=8
+NODE_GPUS=8
+PP=4
+TP=4
 
-GPT_ARGS="--num-layers 24 \
-          --hidden-size 1024 \
-          --num-attention-heads 16 \
-          --seq-length 1024 \
-          --max-position-embeddings 1024 \
-          --micro-batch-size 1 \
-          --global-batch-size 1 \
+GPT_ARGS="--num-layers 32 \
+          --hidden-size 4096 \
+          --num-attention-heads 32 \
+          --seq-length 2048 \
+          --max-position-embeddings 2048 \
           --lr 0.00015 \
-          --train-iters 200 \
+          --train-iters 10 \
           --lr-decay-iters 320000 \
           --lr-decay-style cosine \
           --lr-warmup-fraction .01 \
@@ -26,29 +32,38 @@ GPT_ARGS="--num-layers 24 \
           --no-bias-dropout-fusion \
           --no-async-tensor-model-parallel-allreduce \
           --no-gradient-accumulation-fusion \
+          --checkpoint-activations \
+          --log-interval 1 \
           --num-workers 0"
 
-DISTRIBUTED_ARGS="--nproc_per_node $GPUS \
-                  --nnodes 1 \
-                  --node_rank 0 \
-                  --master_addr localhost \
-                  --master_port 6000"
+# --checkpoint-activations
+
+SINGLE_NODE="--nproc_per_node $NODE_GPUS \
+             --nnodes 1 \
+             --node_rank 0 \
+             --master_addr localhost \
+             --master_port 6000"
+
+MULTI_NODE="--nproc_per_node $NODE_GPUS \
+            --nnodes 2 \
+            --node_rank ${NODE_RANK} \
+            --master_addr worker-0 \
+            --master_port 6012" 
 
 
 cd Megatron-LM
 
-OMP_NUM_THREADS=4 python -m torch.distributed.launch $DISTRIBUTED_ARGS \
+OMP_NUM_THREADS=4 python -m torch.distributed.launch $MULTI_NODE \
        pretrain_gpt_synthetic.py $GPT_ARGS \
-       --tensor-model-parallel-size ${GPUS}\
-       --pipeline-model-parallel-size 1 \
-       --DDP-impl torch
+       --global-batch-size 128 \
+       --micro-batch-size 4 \
+       --tensor-model-parallel-size $TP \
+       --pipeline-model-parallel-size $PP \
+       --DDP-impl local
+
 
 # OMP_NUM_THREADS=4 python -m torch.distributed.launch \
-#        --nproc_per_node 1 \
-#        --nnodes 1 \
-#        --node_rank 0 \
-#        --master_addr localhost \
-#        --master_port 6000 \
+#        --nproc_per_node 1 --master_addr localhost --master_port 6112 \
 #        pretrain_gpt_synthetic.py -h
 
 cd ..
