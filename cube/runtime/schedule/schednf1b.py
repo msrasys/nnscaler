d769ae25f139f33c69b657bea82b9373ec20cb37
Zhiqi Lin <v-zhiql@microsoft.com>
2022-12-04 20:44:11 +0800

    new schedule nf1b

--- cube/runtime/schedule/schednf1b.py
+++ cube/runtime/schedule/schednf1b.py
@@ -14,16 +14,18 @@ from cube.runtime.schedule.strategy import ScheduleABC
 
 
 def first_stage_rfadapter(shapes: Tuple[List[int]], dtypes: Tuple[List[torch.dtype]], dataloader):
-    return next(dataloader)
+    outputs = next(dataloader)
+    outputs = tuple(t.clone() for t in outputs)
+    return outputs
 
-def last_stage_sfadapter(msa_repr: torch.Tensor, pair_repr: torch.Tensor):
+def last_stage_sfadapter(*msa_repr_and_pair_repr: torch.Tensor):
     pass
 
 
 class ScheduleNF1B(ScheduleABC):
 
     @staticmethod
-    def run(segment: Callable,   # forward body
+    def _deprecate_run(segment: Callable,   # forward body
             rfadapter: Callable, # recv_forward adapter
             sfadapter: Callable, # send_forward adapter
             rbadapter: Callable, # recv_backward adapter
@@ -49,6 +51,7 @@ class ScheduleNF1B(ScheduleABC):
                 input_grads = ScheduleNF1B.backward_step(inputs, outputs, (None,))
             for reducer in reducers:
                 reducer()
+            print(f'> rank [{torch.distributed.get_rank()}]: {ScheduleNF1B._fw_cnt}')
             return
 
         # =============================== recycle ====================================
@@ -83,6 +86,7 @@ class ScheduleNF1B(ScheduleABC):
         if stage_id == 0:
             for mid in range(num_microbatch):
                 inputs = ScheduleNF1B.adapter_step(rfadapter, require_grad=False)
+                inputs = (t.cpu() for t in inputs)
                 datas.append(inputs)
         # ==========================================================================
         
@@ -98,6 +102,7 @@ class ScheduleNF1B(ScheduleABC):
             # print(f'rank[{torch.distributed.get_rank()}]: line26: recving forward')
             inputs = ScheduleNF1B.adapter_step(rfadapter, True)
             inputs = datas.pop(0) if inputs == (None,) else inputs
+            inputs = tuple(t.cuda() for t in inputs)
             # forward
             ScheduleNF1B.push_tail('inputs', inputs)
             outputs = ScheduleNF1B.forward_step(segment, *inputs)
@@ -134,6 +139,7 @@ class ScheduleNF1B(ScheduleABC):
                 # print(f'rank[{torch.distributed.get_rank()}]: line77 send backward recv forward')
                 inputs = ScheduleNF1B.exchange(sbadapter, rfadapter, stage_id, (False, True), *input_grads)
                 inputs = datas.pop(0) if inputs == (None,) else inputs
+                inputs = tuple(t.cuda() for t in inputs)
             else:
                 # send backward
                 # print(f'rank[{torch.distributed.get_rank()}]: line82 send backward')
@@ -159,3 +165,128 @@ class ScheduleNF1B(ScheduleABC):
 
         assert len(datas) == 0
         ScheduleNF1B.assert_empty()
+
+
+    @staticmethod
+    def run(segment: Callable,   # forward body
+            rfadapter: Callable, # recv_forward adapter
+            sfadapter: Callable, # send_forward adapter
+            rbadapter: Callable, # recv_backward adapter
+            sbadapter: Callable, # send_backward adapter
+            dataloader: Iterable,
+            stage_id: int,
+            num_stages: int,
+            num_microbatch: int,
+            recycle: int,
+            reducers: List[Callable]):
+        
+        assert num_microbatch >= num_stages
+
+        # special case: num_stages == 1: use gradient accum
+        if num_stages == 1:
+            for _ in range(num_microbatch):
+                inputs = ScheduleNF1B.dataloader_step(dataloader)
+                for _ in range(recycle):
+                    # FIXME: a simulation as output will be loss
+                    with torch.no_grad():
+                        _ = ScheduleNF1B.forward_step(segment, *inputs)
+                outputs = ScheduleNF1B.forward_step(segment, *inputs)
+                input_grads = ScheduleNF1B.backward_step(inputs, outputs, (None,))
+            for reducer in reducers:
+                reducer()
+            # print(f'> rank [{torch.distributed.get_rank()}]: {ScheduleNF1B._fw_cnt}')
+            return
+
+        # setup dummpy adapter
+        if stage_id == 0:
+            assert rfadapter is None
+            shapes, dtypes = [], []
+            for data in ScheduleNF1B.dataloader_step(dataloader):
+                shapes.append(list(data.size()))
+                dtypes.append(data.dtype)
+            rfadapter = partial(first_stage_rfadapter, shapes=shapes, dtypes=dtypes, dataloader=dataloader)
+        if stage_id == num_stages - 1:
+            assert sfadapter is None
+            sfadapter = last_stage_sfadapter
+    
+        # =============================== warmup ========================
+        for rid in range(recycle):
+            # forward rid micro-batches
+            for t in range(rid+1):
+                inputs = ScheduleNF1B.adapter_step(rfadapter, False)
+                inputs = ScheduleNF1B.dataloader_step(dataloader) if inputs == (None,) else inputs
+                with torch.no_grad():
+                    outputs = ScheduleNF1B.forward_step(segment, *inputs)
+                ScheduleNF1B.adapter_step(sfadapter, False, *outputs)
+
+        # print(f'> rank [{torch.distributed.get_rank()}]: OK here')
+        
+        # recv inputs
+        inputs = ScheduleNF1B.adapter_step(rfadapter, stage_id != 0)
+
+        # steady pattern
+        for fmid in range(num_microbatch + num_stages - 1 - stage_id):
+
+            # ======================= forward region ====================
+            if fmid + 1 < num_microbatch:
+                with torch.no_grad():
+                    outputs = ScheduleNF1B.forward_step(segment, *inputs)
+            
+            # ==================  send forward recv backward ==================
+            send_fw = fmid + 1 < num_microbatch
+            bmid = fmid - (num_stages - 1 - stage_id)
+            recv_bw = 0 <= bmid and bmid < num_microbatch
+            if send_fw and recv_bw:
+                grads = ScheduleNF1B.exchange(sfadapter, rbadapter, stage_id, (False, False), *outputs)
+            elif send_fw:
+                ScheduleNF1B.adapter_step(sfadapter, False, *outputs)
+            elif recv_bw:
+                grads = ScheduleNF1B.adapter_step(rbadapter, False)
+            else:
+                assert False, f"> rank [{torch.distributed.get_rank()}]: Fail at fmid: {fmid}"
+
+            # ===================== backward region ==================
+
+            # recycle inference
+            for idx in range(recycle - 1):
+                if fmid + 2 + idx < num_microbatch:
+                    # recv forward
+                    inputs = ScheduleNF1B.adapter_step(rfadapter, False)
+                    # forward
+                    with torch.no_grad():
+                        outputs = ScheduleNF1B.forward_step(segment, *inputs)
+                    # send forward
+                    ScheduleNF1B.adapter_step(sfadapter, False, *outputs)
+            
+            # train forward
+            if fmid < num_microbatch:
+                # recv forward
+                inputs = ScheduleNF1B.adapter_step(rfadapter, stage_id != 0)
+                # forward
+                ScheduleNF1B.push_tail('inputs', inputs)
+                outputs = ScheduleNF1B.forward_step(segment, *inputs)
+                ScheduleNF1B.push_tail('outputs', outputs)
+                # send forward
+                ScheduleNF1B.adapter_step(sfadapter, True, *outputs)
+
+            # train backward
+            bmid = fmid - (num_stages - 1 - stage_id)
+            if 0 <= bmid and bmid < num_microbatch:
+                inputs, outputs = ScheduleNF1B.pop_head('inputs'), ScheduleNF1B.pop_head('outputs')
+                input_grads = ScheduleNF1B.backward_step(inputs, outputs, grads)
+
+            # =============== send backward recv forward =====================
+            send_bw = 0 <= bmid and bmid < num_microbatch
+            recv_fw = fmid + 2 < num_microbatch
+            if send_bw and recv_fw:
+                ScheduleNF1B.exchange(sbadapter, rfadapter, stage_id, (False, False), *input_grads)
+            elif send_bw:
+                ScheduleNF1B.adapter_step(sbadapter, False, *input_grads)
+            elif recv_fw:
+                inputs = ScheduleNF1B.adapter_step(rfadapter, False)
+
+        for reducer in reducers:
+            reducer()
+
+        ScheduleNF1B.assert_empty()
+        # print(f'> rank [{torch.distributed.get_rank()}]: {ScheduleNF1B._fw_cnt}')
