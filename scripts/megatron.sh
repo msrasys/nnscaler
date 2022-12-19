5ee74efdb0e19c63e3d0b3e7f49272c415784ce6
tntnnlrw <tntnnlrw@163.com>
2022-12-19 07:46:35 +0000

    refine scripts

--- scripts/megatron.sh
+++ scripts/megatron.sh
@@ -1,2 +1,2 @@
 torchrun --nproc_per_node=2  --nnodes=1    \
-        examples/nlp/gpt/train.py --policy=PASMegatronWSRTP --lrw --fp16 | tee -a LogForMegatronRecompute.txt
+        examples/nlp/gpt/train.py --policy=PASMegatronWSRTP --fp16 | tee -a LogForMegatronRecompute.txt
---
+++ scripts/pre_install.sh
@@ -0,0 +1,2 @@
+pip install -r requirements.txt
+sudo /opt/conda/bin/python setup.py develop
