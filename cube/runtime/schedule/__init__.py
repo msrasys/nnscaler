8ebd429d3c9b6acb53a5a294c4ea54d17d3d0a77
Zhiqi Lin <v-zhiql@microsoft.com>
2023-01-30 17:27:05 +0800

    add predefined gpipe inference

--- cube/runtime/schedule/__init__.py
+++ cube/runtime/schedule/__init__.py
@@ -1,3 +1,4 @@
 from cube.runtime.schedule.sched1f1b import Schedule1F1B
 from cube.runtime.schedule.schedmix import ScheduleMix
 from cube.runtime.schedule.schednf1b import ScheduleNF1B
+from cube.runtime.schedule.schedinfer import ScheduleInfer
