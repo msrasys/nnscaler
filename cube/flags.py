c51e5c1ed1799a82266bc15d8d451339167e90e1
Zhiqi Lin <v-zhiql@microsoft.com>
2023-02-22 19:15:51 +0800

    fix flag bug: allow to use 0/1 for on/off

--- cube/flags.py
+++ cube/flags.py
@@ -5,35 +5,42 @@ Environment flags for compiling options
 import os
 
 
+def _to_bool(s: str) -> bool:
+    val = os.environ.get(s, default=0)
+    return bool(int(val))
+
+
+def _to_int(s: str, default=0) -> int:
+    val = os.environ.get(s, default=default)
+    return int(val)
+
+
 class CompileFlag:
 
     # ============= loggings ===================
-    log_transform = os.environ.get('LOG_TRANSFORM')
-    log_schedule = os.environ.get('LOG_SCHEDULE')
+    log_transform = _to_bool('LOG_TRANSFORM')
+    log_schedule = _to_bool('LOG_SCHEDULE')
 
-    
     # ================ compiling ========================
     # worker sleep in seconds
-    worker_sleep = int(os.environ.get('WORKER_SLEEP')) if os.environ.get('WORKER_SLEEP') is not None else 0
-    disable_intra_rvd = os.environ.get('DISABLE_INTRA_RVD')
-    disable_inter_rvd = os.environ.get('DISABLE_INTER_RVD')
-    disable_comm_fusion = os.environ.get('DISABLE_COMM_FUSION')
+    worker_sleep = _to_int('WORKER_SLEEP')
+    disable_intra_rvd = _to_bool('DISABLE_INTRA_RVD')
+    disable_inter_rvd =  _to_bool('DISABLE_INTER_RVD')
+    disable_comm_fusion = _to_bool('DISABLE_COMM_FUSION')
 
-    visualize_plan = bool(os.environ.get('VISUALIZE_PLAN'))
+    visualize_plan = _to_bool('VISUALIZE_PLAN')
 
     # ============ code generation ===============
-    use_nnfusion = os.environ.get('USE_NNFUSION')
-    use_jit = os.environ.get('USE_JIT')
-
+    use_nnfusion = _to_bool('USE_NNFUSION')
+    use_jit = _to_bool('USE_JIT')
 
     # ============== runtime ====================
-    dev_mode = os.environ.get('SINGLE_DEV_MODE')  # allow to use python xx.py
+    dev_mode = _to_bool('SINGLE_DEV_MODE')  # allow to use python xx.py
 
     # maximal reducer weight bytes for one allreduce
-    max_reducer_bucket = int(os.environ.get('MAX_REDUCER_BUCKET', default=5e8))
+    max_reducer_bucket = _to_int('MAX_REDUCER_BUCKET', default=5e8)
     
     # use automate mixture precision training, where weights, gradients
     # and optimizer status are kept in its original data type (can be float32),
     # but some of the forward operators will be converted to float16.
-    use_amp = True if os.environ.get('USE_AMP') else False
-
+    use_amp = _to_bool('USE_AMP')
