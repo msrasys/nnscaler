01f1a76682df3e89dd0c2e04c3be22b0c9930d88
Yi Zhu <yizhu1@microsoft.com>
2023-02-28 19:10:19 -0800

    update example code

--- examples/mlp/linearsfx.py
+++ examples/mlp/linearsfx.py
@@ -63,7 +63,7 @@ class MLP(nn.Module):
         for layer in self.layers:
             x = layer(x)
         # x = self.layer_norm(x)
-        x = x.unsqueeze(0)
+        x = x.unsqueeze(1)
         x = self.drop_out(x)
         x = x.squeeze()
         # x = torch.nn.functional.dropout(x, self.p)
