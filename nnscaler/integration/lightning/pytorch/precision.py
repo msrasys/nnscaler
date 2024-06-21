Merged PR 2180: lightning: fix gradient sync and gradient averaging

nnscaler/integration/lightning/pytorch/precision.py
@@ -2,6 +2,7 @@ from typing import TYPE_CHECKING, Any, Callable, ContextManager, Dict, Literal, 
 
 import torch
 from torch import Tensor
+import torch.distributed
 from torch.optim import Optimizer
 import torch.amp
 
@@ -16,7 +17,7 @@ from lightning.fabric.utilities.types import Steppable
 from lightning.pytorch.utilities import GradClipAlgorithmType
 
 
-_PRECISION_INPUT = Literal["32-true", "16-true", "bf16-true"]
+_PRECISION_INPUT = Literal["32-true", "16-true", "bf16-true", "16-mixed", "bf16-mixed"]
 
 
 class NnScalerPrecision(Precision):
@@ -25,7 +26,8 @@ class NnScalerPrecision(Precision):
     .. warning::  This is an :ref:`experimental <versioning:Experimental API>` feature.
 
     Args:
-        precision: Full precision (32-true), half precision (16-true, bf16-true)
+        precision: Full precision (32-true), half precision (16-true, bf16-true) or
+            mixed precision (16-mixed, bf16-mixed).
 
     Raises:
         ValueError:
@@ -56,6 +58,8 @@ class NnScalerPrecision(Precision):
         self.scaler = scaler
 
         precision_to_type = {
+            "bf16-mixed": torch.float32,
+            "16-mixed": torch.float32,
             "bf16-true": torch.bfloat16,
             "16-true": torch.float16,
             "32-true": torch.float32,
@@ -80,7 +84,9 @@ class NnScalerPrecision(Precision):
 
     @override
     def forward_context(self) -> ContextManager:
-        return _DtypeContextManager(self._desired_input_dtype)
+        if "mixed" in self.precision:
+            return torch.autocast("cuda", dtype=(torch.bfloat16 if self.precision == "bf16-mixed" else torch.float16))
+        return self.tensor_init_context()
 
     @override
     def convert_input(self, data: Any) -> Any:
@@ -97,6 +103,17 @@ class NnScalerPrecision(Precision):
         return super().pre_backward(tensor, module)
 
     @override
+    def _after_closure(self, model: "pl.LightningModule", optimizer: Steppable) -> None:
+        if self.scaler is None:  # will be handled in optimizer_step instead of here when using scaler
+            self._sync_grad(model, optimizer)
+        super()._after_closure(model, optimizer)
+
+    def _sync_grad(self, model: "pl.LightningModule", optimizer: Steppable):
+        optimizer.sync_shard_grad()  # closure is used, so we have to sync gradients after closure
+        cf = model._trainer.strategy.compute_config
+        optimizer.scale_grads(cf.plan_ngpus / cf.runtime_ngpus)
+
+    @override
     def optimizer_step(  # type: ignore[override]
         self,
         optimizer: Steppable,
@@ -107,23 +124,26 @@ class NnScalerPrecision(Precision):
         if self.scaler is None:
             return super().optimizer_step(optimizer, model=model, closure=closure, **kwargs)
 
-        closure_result = closure()
+        # TODO: test the following logic
 
-        if not _optimizer_handles_unscaling(optimizer):
-            # Unscaling needs to be performed here in case we are going to apply gradient clipping.
-            # Optimizers that perform unscaling in their `.step()` method are not supported (e.g., fused Adam).
-            # Note: `unscale` happens after the closure is executed, but before the `on_before_optimizer_step` hook.
-            self.scaler.unscale_(optimizer)  # type: ignore[arg-type]
+        closure_result = closure()
+        self._sync_grad(model, optimizer)
+        # Note: `unscale` happens after the closure is executed, but before the `on_before_optimizer_step` hook.
+        # Unscaling needs to be performed after grad sync but before gradient clipping
+        self.scaler.unscale_(optimizer)
 
         self._after_closure(model, optimizer)
-        skipped_backward = closure_result is None
-        # in manual optimization, the closure does not return a value
-        if not model.automatic_optimization or not skipped_backward:
-            # note: the scaler will skip the `optimizer.step` if nonfinite gradients are found
-            step_output = self.scaler.step(optimizer, **kwargs)  # type: ignore[arg-type]
-            self.scaler.update()
-            return step_output
-        return closure_result
+
+        if not model.automatic_optimization:
+            raise ValueError("nnscaler does not support manual optimization.")
+        if closure_result is None:
+            # in manual optimization, the closure does not return a value
+            raise ValueError("nnscaler does not support None as the return value of the closure.")
+
+        # note: the scaler will skip the `optimizer.step` if nonfinite gradients are found
+        step_output = self.scaler.step(optimizer, **kwargs)  # type: ignore[arg-type]
+        self.scaler.update()
+        return step_output
 
     def clip_gradients(
         self,
@@ -138,3 +158,14 @@ class NnScalerPrecision(Precision):
             raise ValueError('nnscaler does not support clipping gradients by value.')
         elif gradient_clip_algorithm == GradClipAlgorithmType.NORM:
             optimizer.clip_gnorm(clip_val)  # define in nnscaler
+
+    @override
+    def state_dict(self) -> Dict[str, Any]:
+        if self.scaler is not None:
+            return self.scaler.state_dict()
+        return {}
+
+    @override
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        if self.scaler is not None:
+            self.scaler.load_state_dict(state_dict)

nnscaler/integration/lightning/pytorch/strategy.py
@@ -1,8 +1,9 @@
-from contextlib import nullcontext
+from contextlib import contextmanager, nullcontext
 from functools import partial
 import logging
 from pathlib import Path
 import os
+import types
 from typing import (
     TYPE_CHECKING,
     Any,
@@ -22,9 +23,11 @@ from typing import (
 
 import torch
 from torch import Tensor
+import torch.distributed
 from torch.nn import Module
 from torch.optim import Optimizer
 from typing_extensions import TypeGuard, override
+from torch.utils.data import DataLoader
 
 import lightning.pytorch as pl
 from lightning.pytorch.accelerators import Accelerator, CUDAAccelerator
@@ -55,6 +58,7 @@ from lightning.pytorch.utilities import GradClipAlgorithmType
 
 import nnscaler
 from nnscaler.integration.lightning.utils import inplace_optimizer_fn
+from nnscaler.runtime.device import DeviceGroup
 from .precision import NnScalerPrecision
 
 
@@ -196,14 +200,38 @@ class NnScalerStrategy(ParallelStrategy):
         if self.lightning_module.trainer.gradient_clip_algorithm == GradClipAlgorithmType.VALUE:
             raise MisconfigurationException("nnscaler does not support clipping gradients by value.")
 
+    def _get_dummy_forward_args(self, model: pl.LightningModule) -> Dict[str, Any]:
+        # two options to set the dummy forward arguments
+
+        if hasattr(model, 'dummy_forward_args'):
+            if not isinstance(model.dummy_forward_args, dict):
+                raise ValueError("The `dummy_forward_args` must be a dictionary with forward arguments names as keys.")
+            return model.dummy_forward_args
+
+        if hasattr(model, 'dummy_forward_args_fn'):
+            dummy_forward_args_fn = getattr(model, 'dummy_forward_args_fn')
+            if not callable(dummy_forward_args_fn):
+                raise ValueError("The `dummy_forward_args_fn` must be a callable function.")
+            trainer = model._trainer
+            data_source = trainer.fit_loop._data_source
+            assert data_source is not None, "The `data_source` must be defined in the trainer."
+            assert data_source.instance is not None, "The `instance` must be defined in the data source."
+            with enforce_0_num_worker(DataLoader):
+                dataloader = data_source.dataloader()
+                assert dataloader.num_workers == 0, "The dataloader must have `num_workers=0`."
+                data = next(iter(dataloader))
+                dummy_forward_args = dummy_forward_args_fn(data)
+                if not isinstance(dummy_forward_args, dict):
+                    raise ValueError("The return value of `dummy_forward_args_fn` must be a dictionary with forward arguments names as keys.")
+                return dummy_forward_args
+
+        raise ValueError("The `dummy_forward_args` or `dummy_forward_args_fn` must be defined in the module.")
+
     @override
     def _setup_model(self, model: Module) -> Module:
         """Set up a module for inference (no optimizers).
         """
-        if getattr(model, 'dummy_forward_args', None) is None:
-            raise ValueError("The `dummy_forward_args` must be defined as a property in the module.")
-        if not isinstance(model.dummy_forward_args, dict):
-            raise ValueError("The `dummy_forward_args` must be a dictionary with forward arguments names as keys.")
+        dummy_forward_args = self._get_dummy_forward_args(model)
 
         old_training_flag = model.training
         if not old_training_flag:
@@ -211,7 +239,7 @@ class NnScalerStrategy(ParallelStrategy):
         model.train()  # always use the model in training mode
         pmodule = nnscaler.parallelize(
             model,
-            self.precision_plugin.convert_input(model.dummy_forward_args),
+            self.precision_plugin.convert_input(dummy_forward_args),
             self.pas_policy,
             self.compute_config,
             gen_savedir=self.gen_savedir,
@@ -239,6 +267,24 @@ class NnScalerStrategy(ParallelStrategy):
         model.to(self.root_device)
         # rewrite model forward to parallelized model forward
         model.forward = pmodule.forward
+
+        # patch log function to add sync_dist_group
+        rank = torch.distributed.get_rank()
+        # create all groups
+        plan_ngpus = self.compute_config.plan_ngpus
+        runtime_ngpus = self.compute_config.runtime_ngpus
+        for i in range(plan_ngpus):
+            DeviceGroup().get_group(
+                list(range(i, runtime_ngpus, plan_ngpus))
+            )
+        sync_group = list(range(rank % plan_ngpus, runtime_ngpus, plan_ngpus))
+
+        _old_log = model.log
+        def _new_log(self, *args, **kwargs) -> None:
+            kwargs['sync_dist_group'] = sync_group
+            _old_log(*args, **kwargs)
+        model.log = types.MethodType(_new_log, model)
+        model._old__log = _old_log
 
         return model
 
@@ -496,3 +542,15 @@ class NnScalerStrategy(ParallelStrategy):
 
     def _get_process_group_backend(self) -> str:
         return 'nccl'  # nnscaler only support nccl
+
+
+@contextmanager
+def enforce_0_num_worker(cls) -> Generator[None, None, None]:
+    """Context manager to enforce the number of workers to be 0 in DataLoader."""
+    _old__init__ = cls.__init__
+    def _new__init__(self, *args, **kwargs) -> None:
+        kwargs['num_workers'] = 0
+        _old__init__(self, *args, **kwargs)
+    cls.__init__ = _new__init__
+    yield
+    cls.__init__ = _old__init__

tests/integration/lightning/pytorch/simple_models.py
@@ -6,6 +6,7 @@ from torch.optim.lr_scheduler import LRScheduler
 import torch.nn.functional as F
 from lightning.pytorch import LightningModule
 from torch import Tensor, nn
+from torch.optim.optimizer import Optimizer
 from torchmetrics import Accuracy
 from torch.utils.data import DataLoader, Dataset, IterableDataset, Subset
 
@@ -30,10 +31,12 @@ class ClassificationModel(LightningModule):
         self.train_acc = acc.clone()
         self.valid_acc = acc.clone()
         self.test_acc = acc.clone()
+        self.dummy_forward_args_fn = lambda batch: {"x": batch[0]}
+        self.update_history = []
 
-    @property
-    def dummy_forward_args(self):
-        return {'x': torch.randn(self.batch_size, self.num_features)}
+    # @property
+    # def dummy_forward_args(self):
+    #     return {'x': torch.randn(self.batch_size, self.num_features)}
 
     def forward(self, x):
         x = self.layer_0(x)
@@ -76,6 +79,24 @@ class ClassificationModel(LightningModule):
         assert not self.training
         x, _ = batch
         return self.forward(x)
+
+    def configure_gradient_clipping(self, optimizer: Optimizer, gradient_clip_val, gradient_clip_algorithm) -> None:
+        def _fix_name(name):
+            prefix = 'nnscaler_pmodule.'
+            if name.startswith(prefix):
+                return name[len(prefix):]
+            return name
+        grads = {_fix_name(n): p.grad.cpu() for n, p in self.named_parameters()}
+        weights = {_fix_name(n): p.data.cpu() for n, p in self.named_parameters()}
+        self.update_history.append((grads, weights))
+        return super().configure_gradient_clipping(optimizer, gradient_clip_val, gradient_clip_algorithm)
+
+
+class ClassificationModelWithLRScheduler(ClassificationModel):
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1)
+        return [optimizer], [scheduler]
 
 
 class RandomDictDataset(Dataset):

tests/integration/lightning/pytorch/test_strategy.py
@@ -10,23 +10,27 @@ from lightning.fabric.utilities.cloud_io import _load as pl_load
 import pytest
 from unittest.mock import Mock, patch
 
+import nnscaler
 from nnscaler.parallel import ComputeConfig
 from nnscaler.integration.lightning.pytorch import NnScalerStrategy, NnScalerPrecision
+import nnscaler.runtime
 
 from ....launch_torchrun import launch_torchrun
+from ....utils import init_random
+from ....parallel_module.common import assert_close, assert_equal
 from .simple_datamodules import ClassifDataModule
-from .simple_models import BoringModel, ClassificationModel
+from .simple_models import BoringModel, ClassificationModel, ClassificationModelWithLRScheduler
 
 
 def fit_worker(tmp_path):
     dm = ClassifDataModule()
     model = ClassificationModel()
-    compute_config=ComputeConfig(2, 2)
+    compute_config=ComputeConfig(1, 1)
     trainer = Trainer(
         default_root_dir=tmp_path,
         max_epochs=2,
-        accelerator="gpu", devices=2,
-        gradient_clip_val=2.0,
+        accelerator="gpu", devices=1,
+        gradient_clip_val=None,
         strategy=NnScalerStrategy(compute_config=compute_config, pas_policy='tp', gen_savedir=tmp_path),
         plugins=[NnScalerPrecision('32-true')]
     )
@@ -36,7 +40,7 @@ def fit_worker(tmp_path):
 
 @pytest.mark.skipif(not torch.cuda.is_available() or torch.cuda.device_count() < 2, reason='lack of gpu devices')
 def test_multi_gpu_model_only(tmp_path):
-    launch_torchrun(2, fit_worker, tmp_path)
+    launch_torchrun(1, fit_worker, tmp_path)
 
 
 def ckpt_path_epoch_restored_worker(tmp_path):
@@ -130,3 +134,122 @@ def trainer_accumulate_grad_batches_zero_grad(tmp_path, accumulate_grad_batches)
 @pytest.mark.skipif(not torch.cuda.is_available() or torch.cuda.device_count() < 2, reason='lack of gpu devices')
 def test_trainer_accumulate_grad_batches_zero_grad(tmp_path, accumulate_grad_batches):
     launch_torchrun(2, trainer_accumulate_grad_batches_zero_grad, tmp_path, accumulate_grad_batches)
+
+
+def correctnes_worker_nnscaler(tmp_path, gradient_clip_val, with_lr_scheduler,
+    precision='32-true',
+    with_tp=False, with_empty_scaler=False
+):
+    init_random()
+    dm = ClassifDataModule()
+    init_random()
+    if with_lr_scheduler:
+        model = ClassificationModelWithLRScheduler()
+    else:
+        model = ClassificationModel()
+    if with_tp:
+        compute_config=ComputeConfig(2, 4)
+        policy = 'tp'
+        devices = 4
+    else:
+        compute_config=ComputeConfig(1, 2)
+        policy = 'dp'
+        devices = 2
+    scaler = None
+    if with_empty_scaler or precision == '16-mixed':
+        scaler = torch.cuda.amp.GradScaler(enabled=(precision == '16-mixed'))
+    trainer = Trainer(
+        default_root_dir=tmp_path,
+        max_epochs=2,
+        accelerator="gpu", devices=devices,
+        gradient_clip_val=gradient_clip_val,
+        strategy=NnScalerStrategy(
+            compute_config=compute_config, pas_policy=policy, gen_savedir=tmp_path,
+            instance_name=policy
+        ),
+        plugins=[NnScalerPrecision(precision, scaler=scaler)]
+    )
+    trainer.fit(model, datamodule=dm)
+    return model.update_history, model.nnscaler_pmodule.fullmap
+
+
+def correctnes_worker_ddp(tmp_path, gradient_clip_val, with_lr_scheduler, precision='32-true'):
+    init_random()
+    dm = ClassifDataModule()
+    init_random()
+    if with_lr_scheduler:
+        model = ClassificationModelWithLRScheduler()
+    else:
+        model = ClassificationModel()
+    trainer = Trainer(
+        default_root_dir=tmp_path,
+        precision=precision,
+        max_epochs=2,
+        accelerator="gpu", devices=2,
+        gradient_clip_val=gradient_clip_val,
+        strategy='ddp',
+    )
+    trainer.fit(model, datamodule=dm)
+    return model.update_history
+
+
+@pytest.mark.skipif(not torch.cuda.is_available() or torch.cuda.device_count() < 2, reason='lack of gpu devices')
+@pytest.mark.parametrize("gradient_clip_val", [None, 0.1])  # 0.1 is chosen to only clip the first update
+@pytest.mark.parametrize("with_lr_scheduler", [False, True])
+def test_correctness(tmp_path, gradient_clip_val, with_lr_scheduler):
+    def _merge_results(returns):
+        results = [returns[i][0] for i in range(len(returns))]
+        fullmaps = [returns[i][1] for i in range(len(returns))]
+        weight_results = []
+        grad_results = []
+        for i in range(len(results[0])):
+            weight_results.append(
+                nnscaler.runtime.module.ParallelModule.merge_state_dicts(
+                    fullmaps,
+                    [result[i][1] for result in results]
+                )[0]
+            )
+            grad_results.append(
+                nnscaler.runtime.module.ParallelModule.merge_state_dicts(
+                    fullmaps,
+                    [result[i][0] for result in results]
+                )[0]
+            )
+        return weight_results, grad_results
+
+    # Test 16-mixed with and without gradient clipping
+    # when gradient clipping is on, the following check will fail
+    # TODO: fix the test when gradient clipping is on
+    if not gradient_clip_val:
+        ddp_results = launch_torchrun(2, correctnes_worker_ddp, tmp_path, gradient_clip_val, with_lr_scheduler, '16-mixed')
+
+        nnscaler_returns = launch_torchrun(2, correctnes_worker_nnscaler, tmp_path, gradient_clip_val, with_lr_scheduler, '16-mixed', False, True)
+        nnscaler_merged_weight_results_fp16, nnscaler_merged_grad_results_fp16 = _merge_results(nnscaler_returns)
+
+        for i in range(len(ddp_results[0])):
+            assert_close(nnscaler_merged_weight_results_fp16[i], ddp_results[0][i][1])
+            assert_close(nnscaler_merged_grad_results_fp16[i], ddp_results[0][i][0])
+            assert_equal(ddp_results[1][i], ddp_results[0][i])
+
+    nnscaler_returns = launch_torchrun(2, correctnes_worker_nnscaler, tmp_path, gradient_clip_val, with_lr_scheduler)
+    nnscaler_merged_weight_results, nnscaler_merged_grad_results = _merge_results(nnscaler_returns)
+
+    nnscaler_returns = launch_torchrun(2, correctnes_worker_nnscaler, tmp_path, gradient_clip_val, with_lr_scheduler, '32-true', False, True)
+    nnscaler_merged_weight_results_scaler, nnscaler_merged_grad_results_scaler = _merge_results(nnscaler_returns)
+    for i in range(len(nnscaler_merged_weight_results_scaler)):
+        assert_equal(nnscaler_merged_weight_results[i], nnscaler_merged_weight_results_scaler[i])
+        assert_equal(nnscaler_merged_grad_results[i], nnscaler_merged_grad_results_scaler[i])
+
+    ddp_results = launch_torchrun(2, correctnes_worker_ddp, tmp_path, gradient_clip_val, with_lr_scheduler)
+    for i in range(len(ddp_results[0])):
+        assert_close(nnscaler_merged_weight_results[i], ddp_results[0][i][1])
+        assert_close(nnscaler_merged_grad_results[i], ddp_results[0][i][0])
+        assert_equal(ddp_results[1][i], ddp_results[0][i])
+
+    if torch.cuda.device_count() >= 4:
+        nnscaler_returns = launch_torchrun(4, correctnes_worker_nnscaler, tmp_path, gradient_clip_val, with_lr_scheduler, '32-true', True)
+        nnscaler_merged_weight_results, nnscaler_merged_grad_results = _merge_results(nnscaler_returns)
+
+        for i in range(len(ddp_results[0])):
+            assert_close(nnscaler_merged_weight_results[i], ddp_results[0][i][1])
+            assert_close(nnscaler_merged_grad_results[i], ddp_results[0][i][0])