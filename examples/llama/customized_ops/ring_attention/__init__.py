37306aa4b0293743274a5021b740188397426f40
zyeric <619828575@qq.com>
2025-10-25 13:36:54 +0800

    remove useless files

--- examples/llama/customized_ops/ring_attention/__init__.py
+++
@@ -1,8 +0,0 @@
-#  Copyright (c) Microsoft Corporation.
-#  Licensed under the MIT License.
-
-from .ring_attn_varlen import wrap_ring_attn_varlen_func
-
-from .zigzag_attn import wrap_zigzag_attn_func
-
-from .ring_attn import wrap_ring_attn_func
