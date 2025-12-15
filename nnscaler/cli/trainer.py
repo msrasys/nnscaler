bb6a8191e7dbe7f7e69abaf32c0a1e5896ca37d2
Yi Zhu <yizhu1@microsoft.com>
2025-12-15 07:23:30 +0000

    Merged PR 2424: [Misc] Refine trainer interfaces
    - refine autodist's check: only double check the follow relation when source and target op have backward data flow
    - update interfaces for hooks: on_train_step_start and on_train_step_end to enable user to change contents in inputs and outpus, which is useful in moe training
    - add `keep_opt` option in merge_checkpoints: when evaluating a large model (like 30B), user does not want the large optimizer part. This option helps to save the cpu memory and execution time.

--- nnscaler/cli/trainer.py
+++ nnscaler/cli/trainer.py
@@ -255,8 +255,14 @@ class Trainer:
         self.hook.after_setup(self)
 
     @classmethod
-    def _merge_checkpoint(cls, checkpoint_files: List[str]):
-        state_dicts = [Checkpointer.load(f) for f in checkpoint_files]
+    def _merge_checkpoint(cls, checkpoint_files: List[str], *, model_only: bool = False):
+        state_dicts = []
+        for f in checkpoint_files:
+            state_dict = Checkpointer.load(f)
+            if model_only:
+                # we pop optimizer state to save cpu memory
+                state_dict.pop('optimizer', None)
+            state_dicts.append(state_dict)
         for i in range(1, len(state_dicts)):
             if state_dicts[i]['train_args'] != state_dicts[0]['train_args']:
                 raise ValueError(f"train_args in {checkpoint_files[i]} is different from {checkpoint_files[0]}")
@@ -265,8 +271,10 @@ class Trainer:
 
         module_state_dict, opt_state_dict = nnscaler.merge_state_dicts(
             [s['model'] for s in state_dicts],
-            [s['optimizer'] for s in state_dicts]
+            [s['optimizer'] for s in state_dicts] if not model_only else None,
         )
+        if model_only:
+            return {'model': module_state_dict}
         train_args = copy.deepcopy(state_dicts[0]['train_args'])
         train_args['checkpoint']['save_type'] = 'merged'
 
@@ -369,8 +377,8 @@ class Trainer:
         return state_dict
 
     @classmethod
-    def merge_checkpoint(cls, checkpoint_files: List[str], output_file: str):
-        merged_state_dict = cls._merge_checkpoint(checkpoint_files)
+    def merge_checkpoint(cls, checkpoint_files: List[str], output_file: str, *, model_only: bool = False):
+        merged_state_dict = cls._merge_checkpoint(checkpoint_files, model_only=model_only)
         Checkpointer.save(merged_state_dict, output_file)
 
     def _log_finalize(self):
@@ -857,7 +865,7 @@ class Trainer:
         torch.cuda.reset_peak_memory_stats()
 
         if self.train_status.finished_train_steps >= self.max_train_steps:
-            logger.info(f"Training is skipped: already done.")
+            logger.info(f"Training is skipped: already done, finished_train_steps={self.train_status.finished_train_steps} >= max_train_steps={self.max_train_steps}.")
             return
 
         start_epoch = self.train_status.finished_train_steps // self.total_train_steps_per_epoch
