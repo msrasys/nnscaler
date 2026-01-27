75ff9f98bc131821309f89f913f6b6749da6dcb1
Xu Weijiang <weijiangxu@microsoft.com>
2026-01-27 08:40:06 +0800

    [Trace] einops support

--- nnscaler/graph/parser/external/einops.py
+++ nnscaler/graph/parser/external/einops.py
@@ -10,9 +10,24 @@ _logger = logging.getLogger(__name__)
 
 try:
     import einops
-
     # trigger einops initialization
     einops.rearrange(torch.arange(1), '(a b c) -> a b c', a=1, b=1, c=1)
+
+    from nnscaler.graph.tracer.wrap_utils import default_never_wrap_function, LeafWrapInfo, Location
+
+    default_never_wrap_function[einops.einops._prepare_transformation_recipe] = \
+        LeafWrapInfo([Location(einops.einops, '_prepare_transformation_recipe')], False, None)
+
+    # we comment out these two functions
+    # because it looks not necessary for now.
+    # and they also introduce some problems,
+    # i.e. dynamic shape will be lost even with `compute_config.constant_folding=False`
+
+    # default_never_wrap_function[einops.einops._reconstruct_from_shape_uncached] = \
+    #     LeafWrapInfo([Location(einops.einops, '_reconstruct_from_shape_uncached')], False, None)
+    # default_never_wrap_function[einops.einops._reconstruct_from_shape] = \
+    #     LeafWrapInfo([Location(einops.einops, '_reconstruct_from_shape')], False, None)
+
 except ImportError as e:
     _logger.debug("Einops is not installed")
     pass
--- tests/parallel_module/test_gencode_einops.py
+++ tests/parallel_module/test_gencode_einops.py
@@ -5,6 +5,7 @@ import tempfile
 import functools
 from einops import rearrange
 import torch
+import pytest
 
 from nnscaler import parallelize, ComputeConfig
 from nnscaler.graph import parser
@@ -65,3 +66,66 @@ def test_codegen_rearrange():
         )
         # parallelize will succeed.
         assert True
+
+
+class RearrangeModule2(torch.nn.Module):
+    def __init__(self, shape: tuple[int, ...]):
+        super().__init__()
+        self.shape = shape
+        self.weight = torch.nn.Parameter(torch.ones(self.shape))
+
+    def forward(self, x):
+        bsz = x.size(0)
+        x = rearrange(x, 'n l d -> (n l) d', n=bsz)
+        return x + self.weight
+
+
+@replace_all_device_with('cpu')
+@pytest.mark.parametrize("constant_folding", [True, False])
+def test_rearrange2(tmp_path, constant_folding):
+    parallelize(
+        RearrangeModule2(4),
+        {'x': torch.randn(4, 4, 4)},
+        'dp',
+        ComputeConfig(1, 1, constant_folding=constant_folding),
+        gen_savedir=tmp_path,
+        load_module=False
+    )
+    # parallelize will succeed.
+    assert True
+
+    # code will look like this when constant_folding=True
+    # def segment22(self, x_25):
+    #     # File "/data/weijiangxu/uvenv/kosmos3.10/lib/python3.10/site-packages/einops/_backends.py", line 93, in reshape,  return x.reshape(shape)
+    #     reshape_27 = torch.Tensor.reshape(x_25, shape=(16, 4))
+    #     del x_25
+    #     # File "/data/weijiangxu/nnscaler/tests/parallel_module/test_gencode_einops.py", line 80, in forward,  return x + self.weight
+    #     add_26 = torch.add(reshape_27, self.weight_28, alpha=1)
+    #     del reshape_27
+    #     return add_26
+
+
+    # code will look like this when constant_folding=False
+    # def segment25(self, x_28):
+    #     # File "/data/weijiangxu/nnscaler/tests/parallel_module/test_gencode_einops.py", line 78, in forward,  bsz = x.size(0)
+    #     size_21 = torch.Tensor.size(x_28, dim=0)
+    #     # File "/data/weijiangxu/uvenv/kosmos3.10/lib/python3.10/site-packages/einops/_backends.py", line 90, in shape,  return x.shape
+    #     im_output_36 = builtins.getattr(x_28, 'shape')
+    #     getattr_3_22 = im_output_36[0]
+    #     getattr_3_23 = im_output_36[1]
+    #     getattr_3_24 = im_output_36[2]
+    #     del im_output_36
+    #     # File "/data/weijiangxu/uvenv/kosmos3.10/lib/python3.10/site-packages/einops/einops.py", line 33, in _product,  result *= element
+    #     mul_1_25 = _operator.mul(1, size_21)
+    #     # File "/data/weijiangxu/uvenv/kosmos3.10/lib/python3.10/site-packages/einops/einops.py", line 33, in _product,  result *= element
+    #     imul_26 = _operator.imul(mul_1_25, getattr_3_23)
+    #     # File "/data/weijiangxu/uvenv/kosmos3.10/lib/python3.10/site-packages/einops/einops.py", line 33, in _product,  result *= element
+    #     mul_2_27 = _operator.mul(1, getattr_3_24)
+    #     # File "/data/weijiangxu/uvenv/kosmos3.10/lib/python3.10/site-packages/einops/_backends.py", line 93, in reshape,  return x.reshape(shape)
+    #     reshape_30 = torch.Tensor.reshape(x_28, shape=(imul_26, mul_2_27))
+    #     del x_28
+    #     # File "/data/weijiangxu/nnscaler/tests/parallel_module/test_gencode_einops.py", line 80, in forward,  return x + self.weight
+    #     add_29 = torch.add(reshape_30, self.weight_31, alpha=1)
+    #     del reshape_30
+    #     return add_29
+
