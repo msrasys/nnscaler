25f66c6d90c71365a5d4e5c979a8af45f30a1b83
sunyt32 <sunyt32@163.com>
2026-03-30 21:40:43 -0700

    add check status flag

--- nnscaler/cli/trainer.py
+++ nnscaler/cli/trainer.py
@@ -246,6 +246,7 @@ class Trainer:
         *,
         model_only: bool = False,
         checkpointer: Optional[Checkpointer] = None,
+        check_status: bool = True,
     ):
         checkpointer = checkpointer or Checkpointer()
         state_dicts = []
@@ -255,11 +256,12 @@ class Trainer:
                 # we pop optimizer state to save cpu memory
                 state_dict.pop('optimizer', None)
             state_dicts.append(state_dict)
-        for i in range(1, len(state_dicts)):
-            if state_dicts[i]['train_args'] != state_dicts[0]['train_args']:
-                raise ValueError(f"train_args in {checkpoint_files[i]} is different from {checkpoint_files[0]}")
-            if state_dicts[i].get('lr_scheduler', None) != state_dicts[0].get('lr_scheduler', None):
-                raise ValueError(f"lr_scheduler state in {checkpoint_files[i]} is different from {checkpoint_files[0]}")
+        if check_status:
+            for i in range(1, len(state_dicts)):
+                if state_dicts[i]['train_args'] != state_dicts[0]['train_args']:
+                    raise ValueError(f"train_args in {checkpoint_files[i]} is different from {checkpoint_files[0]}")
+                if state_dicts[i].get('lr_scheduler', None) != state_dicts[0].get('lr_scheduler', None):
+                    raise ValueError(f"lr_scheduler state in {checkpoint_files[i]} is different from {checkpoint_files[0]}")
 
         module_state_dict, opt_state_dict = nnscaler.merge_state_dicts(
             [s['model'] for s in state_dicts],
@@ -375,6 +377,7 @@ class Trainer:
         checkpointer: Optional[Checkpointer] = None,
         serializer: Optional[str] = None,
         serializer_args: Optional[dict[str, Any]] = None,
+        check_status: bool = True,
     ):
         if checkpointer is not None:
             if serializer is not None or serializer_args is not None:
@@ -386,6 +389,7 @@ class Trainer:
             checkpoint_files,
             model_only=model_only,
             checkpointer=checkpointer,
+            check_status=check_status,
         )
         checkpointer.save(merged_state_dict, output_file)
         checkpointer.flush()
