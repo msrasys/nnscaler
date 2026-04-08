f7ec38d2b38ad1cda946c331b010fa69b018ef02
Xu Weijiang <weijiangxu@microsoft.com>
2026-04-08 09:30:06 +0800

    refine tests

--- tests/cli/test_trainer_overlap.py
+++ tests/cli/test_trainer_overlap.py
@@ -19,6 +19,7 @@ from nnscaler.policies import get_pas_ops
 from nnscaler.cli.trainer import Trainer
 
 from tests.launch_torchrun import launch_torchrun
+from tests.parallel_module.common import assert_equal
 
 # hyper-parameters
 hidden     = 2048
@@ -296,8 +297,8 @@ def trainer_worker_overlap(save_dir, config_file):
         '--checkpoint.save_dir', str(ckpt_savedir),
     ])
     trainer.run()
-    assert trainer.model.use_scheduler
-    assert trainer.model.nmicros_per_scheduler_step == 4
+    if trainer.model.use_scheduler:
+        assert trainer.model.nmicros_per_scheduler_step == 4
 
     if trainer.rank == 0:
         Trainer.merge_checkpoint(list((ckpt_savedir / 'last').glob('*.ckpt')), save_dir / f'merged_{run_name}.pt')
@@ -308,3 +309,10 @@ def trainer_worker_overlap(save_dir, config_file):
 @pytest.mark.skipif(not torch.cuda.is_available() or torch.cuda.device_count() < 2, reason='lack of gpu devices')
 def test_trainer_overlap(tmp_path):
     launch_torchrun(2, trainer_worker_overlap, tmp_path, 'trainer_args_pipeline_overlap.yaml')
+    launch_torchrun(2, trainer_worker_overlap, tmp_path, 'trainer_args_pipeline_overlap_gt.yaml')
+
+    overlap_ckpt = torch.load(tmp_path / 'merged_trainer_args_pipeline_overlap.pt', weights_only=False)
+    gt_ckpt = torch.load(tmp_path / 'merged_trainer_args_pipeline_overlap_gt.pt', weights_only=False)
+
+    assert_equal(overlap_ckpt['model'], gt_ckpt['model'])
+    assert_equal(overlap_ckpt['optimizer']['state'], gt_ckpt['optimizer']['state'])
