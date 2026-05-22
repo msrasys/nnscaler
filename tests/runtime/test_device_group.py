596b1f6d6a328e667a8050438aa155c360c57209
Xu Weijiang <weijiangxu@microsoft.com>
2026-05-22 13:50:34 +0800

    refine tests

--- tests/runtime/test_device_group.py
+++ tests/runtime/test_device_group.py
@@ -10,23 +10,24 @@ from ..launch_torchrun import launch_torchrun
 def _barrier_worker():
     init()
 
-    with warnings.catch_warnings(record=True) as caught:
-        warnings.simplefilter("always")
-        for _ in range(3):
-            dist.barrier()
+    try:
+        with warnings.catch_warnings(record=True) as caught:
+            warnings.simplefilter("always")
+            for _ in range(3):
+                dist.barrier()
 
-    # make sure there is no barrier() device warning
-    # UserWarning: barrier(): using the device under current context. You can specify device_id in init_process_group to mute this warning.
-    # return func(*args, **kwargs)
-    barrier_warnings = [
-        w for w in caught
-        if issubclass(w.category, UserWarning)
-        and 'barrier()' in str(w.message)
-        and 'device_id' in str(w.message)
-    ]
-    assert len(barrier_warnings) == 0
-
-    uninit()
+        # make sure there is no barrier() device warning
+        # UserWarning: barrier(): using the device under current context. You can specify device_id in init_process_group to mute this warning.
+        # return func(*args, **kwargs)
+        barrier_warnings = [
+            w for w in caught
+            if issubclass(w.category, UserWarning)
+            and 'barrier()' in str(w.message)
+            and 'device_id' in str(w.message)
+        ]
+        assert len(barrier_warnings) == 0
+    finally:
+        uninit()
 
 
 def test_barrier_warning():
